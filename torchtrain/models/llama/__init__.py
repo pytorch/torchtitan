09c32a285bef67020d6b635e0ce45e824018cb3f
Wanchao Liang <wanchaol@users.noreply.github.com>
2024-01-10 00:06:40 -0800

    import llama model

---
+++ torchtrain/models/llama/__init__.py
@@ -0,0 +1,8 @@
+from .model import ModelArgs
+
+llama_configs = {
+    "debugmodel": ModelArgs(dim=256, n_layers=1, n_heads=16),
+    "7B": ModelArgs(dim=4096, n_layers=32, n_heads=32),
+    "13B": ModelArgs(dim=5120, n_layers=40, n_heads=40),
+    "70B": ModelArgs(dim=8192, n_layers=80, n_heads=64, n_kv_heads=8, ffn_dim_multiplier=1.3, multiple_of=4096),
+}
