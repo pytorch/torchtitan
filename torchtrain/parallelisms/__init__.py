2980586a75b1f5f20def917c5295cae7c52abe2c
Wanchao Liang <wanchaol@users.noreply.github.com>
2024-02-01 14:23:10 -0800

    Update dataloading to use packing with fixed seq_length
    This is so that we can improve our training performance
    
    TODO: this is still loading the same data on each rank, which is
    semantically wrong, we should load different chunk of data on each data
    parallel rank
    
    ghstack-source-id: 23b2b301d3ebb38c016efb21fbbde87bdf1772ef
    Pull Request resolved: https://github.com/pytorch-labs/torchtrain/pull/31

--- torchtrain/parallelisms/__init__.py
+++ torchtrain/parallelisms/__init__.py
@@ -1,5 +1,58 @@
+import os
+import logging
+from dataclasses import dataclass
+
+from torch.distributed.device_mesh import init_device_mesh
+
 from torchtrain.parallelisms.parallelize_llama import parallelize_llama
+
+logger = logging.getLogger(__name__)
+
 
 models_parallelize_fns = {
     "llama": parallelize_llama,
 }
+
+
+@dataclass
+class ParallelDims:
+    dp: int
+    sp: int
+    pp: int
+    world_size: int
+
+    def __post_init__(self):
+        self._validate()
+
+    def _validate(self):
+        dp, sp, pp = self.dp, self.sp, self.pp
+        if dp == -1:
+            self.dp = dp = self.world_size // (sp * pp)
+        assert dp >= 1, dp
+        assert sp >= 1, sp
+        assert pp >= 1, pp
+        assert dp * sp * pp == self.world_size, (
+            f"Invalid parallel dims: dp({dp}) * sp({sp}) * pp({pp}) != WORLD_SIZE({self.world_size})"
+        )
+
+    def build_mesh(self, device_type):
+        dims = []
+        names = []
+        for d, name in zip([self.dp, self.sp, self.pp], ["dp", "sp", "pp"]):
+            if d > 1:
+                dims.append(d)
+                names.append(name)
+        logger.info(f"Building {len(dims)}-D device mesh with {names}, {dims}")
+        return init_device_mesh(device_type, dims, mesh_dim_names=names)
+
+    @property
+    def dp_enabled(self):
+        return self.dp > 1
+
+    @property
+    def sp_enabled(self):
+        return self.sp > 1
+
+    @property
+    def pp_enabled(self):
+        return self.pp > 1
