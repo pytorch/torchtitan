196d56e48dc6f407f7bd1b60678279f9f96eadf0
Tianyu Liu <lty@fb.com>
2024-02-14 18:16:16 -0800

    add TensorBoard logging with loss and wps
    ghstack-source-id: d0828f16c06747a5af2586630e5205bf786de1c4
    Pull Request resolved: https://github.com/pytorch-labs/torchtrain/pull/57

--- torchtrain/metrics.py
+++ torchtrain/metrics.py
@@ -4,10 +4,17 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved
 
+import os
 from collections import namedtuple
+from datetime import datetime
+from typing import Any, Dict, Optional
 
 import torch
 import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+
+from torchtrain.logging_utils import rank0_log
+from torchtrain.profiling import get_config_from_toml
 
 _gb_in_bytes = 1024 * 1024 * 1024
 _mb_in_bytes = 1024 * 1024
@@ -187,3 +194,40 @@ def get_num_params(model: nn.Module, only_trainable: bool = False) -> int:
         param_list = [p for p in param_list if p.requires_grad]
     unique_params = {p.data_ptr(): p for p in param_list}.values()
     return sum(p.numel() for p in unique_params)
+
+
+class MetricLogger:
+    def __init__(self, log_dir, tag, enable_tb):
+        self.tag = tag
+        self.writer: Optional[SummaryWriter] = None
+        if enable_tb:
+            self.writer = SummaryWriter(log_dir, max_queue=1000)
+
+    def log(self, metrics: Dict[str, Any], step: int):
+        if self.writer is not None:
+            for k, v in metrics.items():
+                tag = k if self.tag is None else f"{self.tag}/{k}"
+                self.writer.add_scalar(tag, v, step)
+
+    def close(self):
+        if self.writer is not None:
+            self.writer.close()
+
+
+def build_metric_logger(tag: Optional[str] = None):
+    config = get_config_from_toml()
+
+    dump_dir = config["global"]["dump_folder"]
+    save_tb_folder = config["metrics"]["save_tb_folder"]
+    # since we don't have run id yet, use current minute as identifier
+    datetime_str = datetime.now().strftime("%Y%m%d-%H%M")
+    log_dir = os.path.join(dump_dir, save_tb_folder, datetime_str)
+
+    enable_tb = config["metrics"].get("enable_tensorboard", False)
+    if enable_tb:
+        rank0_log(
+            f"Metrics logging active. Tensorboard logs will be saved at {log_dir}."
+        )
+
+    rank_str = f"rank_{torch.distributed.get_rank()}"
+    return MetricLogger(os.path.join(log_dir, rank_str), tag, enable_tb)
