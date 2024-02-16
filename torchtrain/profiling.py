d3454086c0a576963e16429a25dcb16e7a365bd2
lessw2020 <lessw@etrillium.com>
2024-02-16 09:31:20 -0800

    updated license agreement

--- torchtrain/profiling.py
+++ torchtrain/profiling.py
@@ -1,8 +1,9 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 import contextlib
 import os
+
 import torch
 
 try:
