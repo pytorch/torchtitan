555c2ba1f2482948d474a1239b6c9c186292d3d2
gnadathur <gnadathur@devgpu051.cln3.facebook.com>
2024-02-23 09:38:38 -0800

    Config manager supports command line overrides
    Summary:
    PR implements following enhancements to config manager.
    1. Command line args and toml file args are now unified.
    2. Defaults can be loaded from either.
    3. Defaults can be overridden through command line. Overrides will
    be applied irrespective of where the defaults were loaded from.
    
    Test Plan:
    ============================= test session starts ==============================
    platform linux -- Python 3.10.13, pytest-8.0.1, pluggy-1.4.0 -- /home/gnadathur/local/a/pytorch-env/bin/python
    cachedir: .pytest_cache
    rootdir: /data/users/gnadathur/a/torchtrain
    configfile: pyproject.toml
    plugins: cov-4.1.0
    collecting ... collected 5 items
    
    test/test_job_config.py::TestJobConfig::test_command_line_args PASSED    [ 20%]
    test/test_job_config.py::TestJobConfig::test_command_line_args_with_override PASSED [ 40%]
    test/test_job_config.py::TestJobConfig::test_job_config_file PASSED      [ 60%]
    test/test_job_config.py::TestJobConfig::test_job_config_file_with_override PASSED [ 80%]
    test/test_job_config.py::TestJobConfig::test_job_file_does_not_exist PASSED [100%]
    
    ---------- coverage: platform linux, python 3.10.13-final-0 ----------
    Coverage XML written to file coverage.xml
    
    ============================= slowest 20 durations =============================
    0.01s call     test/test_job_config.py::TestJobConfig::test_job_config_file_with_override
    0.00s call     test/test_job_config.py::TestJobConfig::test_job_config_file
    0.00s call     test/test_job_config.py::TestJobConfig::test_command_line_args
    0.00s call     test/test_job_config.py::TestJobConfig::test_command_line_args_with_override
    0.00s call     test/test_job_config.py::TestJobConfig::test_job_file_does_not_exist
    0.00s setup    test/test_job_config.py::TestJobConfig::test_command_line_args
    0.00s teardown test/test_job_config.py::TestJobConfig::test_command_line_args
    0.00s setup    test/test_job_config.py::TestJobConfig::test_job_file_does_not_exist
    0.00s setup    test/test_job_config.py::TestJobConfig::test_command_line_args_with_override
    0.00s teardown test/test_job_config.py::TestJobConfig::test_command_line_args_with_override
    0.00s setup    test/test_job_config.py::TestJobConfig::test_job_config_file_with_override
    0.00s setup    test/test_job_config.py::TestJobConfig::test_job_config_file
    0.00s teardown test/test_job_config.py::TestJobConfig::test_job_file_does_not_exist
    0.00s teardown test/test_job_config.py::TestJobConfig::test_job_config_file
    0.00s teardown test/test_job_config.py::TestJobConfig::test_job_config_file_with_override
    ============================== 5 passed in 0.10s ===============================
    
    Successful 10 iterations llama.sh run
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 9a0f588849f520652a431df438318be3ab0f9578
    Pull Request resolved: https://github.com/pytorch-labs/torchtrain/pull/69

--- test/test_job_config.py
+++ test/test_job_config.py
@@ -2,10 +2,25 @@ import pytest
 from torchtrain.config_manager import JobConfig
 
 class TestJobConfig():
-    def test_job_config(self):
-        config = JobConfig()
+    def test_command_line_args(self):
+        config = JobConfig([])
         assert config.model.name == "llama"
 
-    def test_file_does_not_exist(self):
+    def test_command_line_args_with_override(self):
+        config = JobConfig(["--metrics_log_freq" , "2", "--metrics_enable_tensorboard"])
+        assert config.metrics.log_freq == 2
+        assert config.metrics.enable_tensorboard
+
+    def test_job_config_file(self):
+        config = JobConfig(["--global_config_file", "./torchtrain/train_configs/train_config.toml"])
+        assert config.model.name == "llama"
+
+    def test_job_config_file_with_override(self):
+        config = JobConfig(["--global_config_file",
+                            "./torchtrain/train_configs/train_config.toml",
+                            "--metrics_log_freq" , "2"])
+        assert config.metrics.log_freq == 2
+
+    def test_job_file_does_not_exist(self):
         with pytest.raises(FileNotFoundError):
-            JobConfig("ohno.toml")
+            JobConfig(["--global_config_file", "ohno.toml"])
