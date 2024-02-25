837a58ba6855270d1427c8e074f4bbac82129978
lessw2020 <lessw@etrillium.com>
2024-02-25 12:29:48 -0800

    add minipile dataset for pretraining

--- torchtrain/datasets/__init__.py
+++ torchtrain/datasets/__init__.py
@@ -2,10 +2,15 @@
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 from torchtrain.datasets.alpaca import build_alpaca_data_loader
-from torchtrain.datasets.tokenizer import create_tokenizer
+from torchtrain.datasets.minipile import build_minipile_data_loader
 
-__all__ = ["build_alpaca_data_loader", "create_tokenizer", "pad_batch_to_longest_seq"]
+__all__ = [
+    "build_alpaca_data_loader",
+    "build_minipile_data_loader" "create_tokenizer",
+    "pad_batch_to_longest_seq",
+]
 
 dataloader_fn = {
     "alpaca": build_alpaca_data_loader,
+    "minipile": build_minipile_data_loader,
 }
---
+++ torchtrain/datasets/minipile.py
@@ -0,0 +1,80 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+from typing import List
+
+import torch
+from torch.utils.data import DataLoader, IterableDataset
+
+from torchtrain.datasets.tokenizer import TokenizerIf
+
+from datasets import load_dataset
+from datasets.distributed import split_dataset_by_node
+
+
+class MiniPileDataset(IterableDataset):
+    """PyTorch Representation of the MiniPile Dataset from Hugging Face.
+
+    Args:
+        tokenizer (Tokenizer): Tokenizer used to encode data. Tokenize must implement an `encode` and `decode` method.
+        seq_len (int): max sequence length
+        world_size (int): number of data parallel processes participating in training
+        rank (int): rank of the current data parallel process
+
+    Data input format:
+    {
+        "instruction": "Create a classification task by clustering the given list of items.",
+        "input": "Apples, oranges, bananas, strawberries, pineapples",
+        "output": "Class 1: Apples, Oranges\nClass 2: Bananas, Strawberries\nClass 3: Pineapples",
+        "text": "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\nCreate a classification task by clustering the given list of items.\n\n### Input:\nApples, oranges, bananas, strawberries, pineapples\n\n### Response:\nClass 1: Apples,
+        Oranges\nClass 2: Bananas, Strawberries\nClass 3: Pineapples",  # noqa: B950
+    }
+
+    Example:
+    >>> alpaca_ds = AlpacaDataset(tokenizer=tokenizer)
+    >>> for batch in Dataloader(alpaca_ds, batch_size=8):
+            print(f"Batch size: {len(batch)}")
+        Batch size: 8
+    """
+
+    def __init__(
+        self,
+        tokenizer: TokenizerIf,
+        seq_len: int = 2048,
+        world_size: int = 1,
+        rank: int = 0,
+        **kwargs
+    ) -> None:
+        # TODO: This is a temporary solution for small datasets like Alpaca.
+        #       For larger datasets we need to use a more scalable approach.
+        # Setting `streaming=True` works for large dataset, but the speed is slow.
+        ds = load_dataset("JeanKaddour/minipile", split="train")
+        self.data_iterator = iter(split_dataset_by_node(ds, rank, world_size))
+        self._tokenizer = tokenizer
+        self.seq_len = seq_len
+
+    def __iter__(self):
+        max_buffer_token_len = 1 + self.seq_len
+        all_tokens: List[int] = []
+
+        for sample in self.data_iterator:
+            sample_text = sample["text"]
+            sample_tokens = self._tokenizer.encode(sample_text, bos=True, eos=True)
+            all_tokens.extend(sample_tokens)
+
+            while len(all_tokens) >= max_buffer_token_len:
+                x = torch.LongTensor(all_tokens[:max_buffer_token_len])
+                # batched_x = x.reshape(self.batch_size, -1)
+                # update tokens to the remaining tokens
+                all_tokens = all_tokens[max_buffer_token_len:]
+                input = x[:-1]
+                label = x[1:]
+                yield input, label
+
+
+def build_minipile_data_loader(
+    tokenizer: TokenizerIf, batch_size: int, seq_len: int, world_size, rank
+):
+    minipile_ds = MiniPileDataset(tokenizer, seq_len, world_size, rank)
+
+    return DataLoader(minipile_ds, batch_size=batch_size)
