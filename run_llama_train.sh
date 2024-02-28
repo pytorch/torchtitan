9066b6cdab7f94f593e04ef1dc72f5cde59a4b27
gnadathur <gnadathur@devvm4378.nao0.facebook.com>
2024-02-28 11:47:27 -0800

    add comment for the env variable

--- run_llama_train.sh
+++ run_llama_train.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+# libUV is a scalable backend for TCPStore which is used in processGroup
+# rendezvous. This is the recommended backend for distributed training.
 export USE_LIBUV=1
 TRAINER_DIR=${1:-/home/$USER/local/torchtrain}
 
