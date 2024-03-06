a6e8c054391f3fb6682b7466cd89a56d265511a9
lessw2020 <lessw@etrillium.com>
2024-03-06 04:13:30 -0800

    restore inifinite dataset option

--- torchtrain/datasets/hf_datasets.py
+++ torchtrain/datasets/hf_datasets.py
@@ -34,6 +34,7 @@ class HuggingFaceDataset(IterableDataset):
         seq_len (int): max sequence length
         world_size (int): number of data parallel processes participating in training
         rank (int): rank of the current data parallel process
+        infinite (bool): whether to loop infinitely over the dataset
 
     Data input format (minipile):
     {
@@ -53,8 +54,8 @@ class HuggingFaceDataset(IterableDataset):
     }
 
     Example:
-    >>> minipile_ds = MiniPileDataset(tokenizer=tokenizer)
-    >>> for batch in Dataloader(minipile_ds, batch_size=8):
+    >>> alpaca_ds = HuggingFaceDataset(tokenizer=tokenizer)
+    >>> for batch in Dataloader(alpaca_ds, batch_size=8):
             print(f"Batch size: {len(batch)}")
         Batch size: 8
     """
@@ -66,6 +67,7 @@ class HuggingFaceDataset(IterableDataset):
         seq_len: int = 2048,
         world_size: int = 1,
         rank: int = 0,
+        infinite: bool = False,
     ) -> None:
         # TODO: This is a temporary solution for small datasets like Alpaca.
         #       For larger datasets we need to use a more scalable approach.
@@ -74,29 +76,32 @@ class HuggingFaceDataset(IterableDataset):
             raise ValueError(
                 f"Dataset {dataset_name} is not supported. Supported datasets are: {_supported_datasets.keys()}"
             )
+
         ds = load_dataset(_supported_datasets[dataset_name], split="train")
-        # ds = load_dataset("JeanKaddour/minipile", split="train")
-        self.data_iterator = iter(split_dataset_by_node(ds, rank, world_size))
+        self._data = split_dataset_by_node(ds, rank, world_size)
         self._tokenizer = tokenizer
         self.seq_len = seq_len
+        self.infinite = infinite
 
     def __iter__(self):
         max_buffer_token_len = 1 + self.seq_len
         all_tokens: List[int] = []
 
-        for sample in self.data_iterator:
-            sample_text = sample["text"]
-            sample_tokens = self._tokenizer.encode(sample_text, bos=True, eos=True)
-            all_tokens.extend(sample_tokens)
+        while True:
+            for sample in iter(self._data):
+                sample_text = sample["text"]
+                sample_tokens = self._tokenizer.encode(sample_text, bos=True, eos=True)
+                all_tokens.extend(sample_tokens)
 
-            while len(all_tokens) >= max_buffer_token_len:
-                x = torch.LongTensor(all_tokens[:max_buffer_token_len])
-                # batched_x = x.reshape(self.batch_size, -1)
-                # update tokens to the remaining tokens
-                all_tokens = all_tokens[max_buffer_token_len:]
-                input = x[:-1]
-                label = x[1:]
-                yield input, label
+                while len(all_tokens) >= max_buffer_token_len:
+                    x = torch.LongTensor(all_tokens[:max_buffer_token_len])
+                    # update tokens to the remaining tokens
+                    all_tokens = all_tokens[max_buffer_token_len:]
+                    input = x[:-1]
+                    label = x[1:]
+                    yield input, label
+            if not self.infinite:
+                break
 
 
 def build_hf_data_loader(
@@ -106,7 +111,8 @@ def build_hf_data_loader(
     seq_len: int,
     world_size,
     rank,
+    infinite: bool = True,
 ):
-    hf_ds = HuggingFaceDataset(dataset_name, tokenizer, seq_len, world_size, rank)
+    hf_ds = HuggingFaceDataset(dataset_name, tokenizer, seq_len, world_size, rank, infinite)
 
     return DataLoader(hf_ds, batch_size=batch_size)
