5534b369b4e3e279c8a3b8a59b2f07ac6914045c
lessw2020 <lessw@etrillium.com>
2024-03-06 04:26:45 -0800

    consolidate interface for datasets pkg by integrating create_tokenizer

--- torchtrain/datasets/__init__.py
+++ torchtrain/datasets/__init__.py
@@ -2,6 +2,7 @@
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 from torchtrain.datasets.hf_datasets import build_hf_data_loader
+from torchtrain.datasets.tokenizer import create_tokenizer
 
 __all__ = [
     "build_hf_data_loader",
