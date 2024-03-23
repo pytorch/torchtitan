35a25f466b9cfd829e644e1932e9de084df77623
Will Constable <whc@meta.com>
2024-03-22 17:12:52 -0700

    Update (base update)
    [ghstack-poisoned]

---
+++ torchtrain/datasets/tokenizer.py
@@ -0,0 +1,102 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# copied and adjusted from https://github.com/facebookresearch/llama/blob/main/llama/tokenizer.py
+
+import os
+from abc import ABC, abstractmethod
+from typing import List
+
+from sentencepiece import SentencePieceProcessor
+
+from torchtrain.logging_utils import logger
+
+
+class TokenizerIf(ABC):
+    # tokenizer interface
+    def __init__(self, tokenizer_path: str):
+        assert os.path.exists(
+            tokenizer_path
+        ), f"The tokenizer path does not exist: {tokenizer_path}"
+        assert os.path.isfile(tokenizer_path), tokenizer_path
+        self._n_words = 8
+
+    @abstractmethod
+    def encode(self, *args, **kwargs) -> List[int]:
+        ...
+
+    @abstractmethod
+    def decode(self, *args, **kwargs) -> str:
+        ...
+
+    @property
+    def n_words(self) -> int:
+        return self._n_words
+
+
+def create_tokenizer(tokenizer_type: str, tokenizer_path: str) -> TokenizerIf:
+    logger.info(f"Building {tokenizer_type} tokenizer locally from {tokenizer_path}")
+    if tokenizer_type == "sentencepiece":
+        return SentencePieceTokenizer(tokenizer_path)
+    else:
+        raise ValueError(f"Unknown tokenizer type: {args.type}")
+
+
+class SentencePieceTokenizer(TokenizerIf):
+    """
+    Tokenizing and encoding/decoding text based on a SentencePiece model.
+
+    Args:
+        tokenizer_path (str): The path to the SentencePiece model file.
+    """
+
+    def __init__(self, tokenizer_path: str):
+
+        super().__init__(tokenizer_path)
+        # reload tokenizer
+        self.sp_model = SentencePieceProcessor(model_file=tokenizer_path)
+
+        # BOS / EOS token IDs
+        self._n_words: int = self.sp_model.vocab_size()
+        self.bos_id: int = self.sp_model.bos_id()
+        self.eos_id: int = self.sp_model.eos_id()
+        self.pad_id: int = self.sp_model.pad_id()
+        logger.info(
+            f"SentencePieceTokenizer built: #words {self.n_words}, BOS ID {self.bos_id}, EOS ID {self.eos_id}"
+        )
+        assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
+
+    def encode(self, s: str, bos: bool, eos: bool) -> List[int]:
+        """
+        Encodes a string into a list of token IDs.
+
+        Args:
+            s (str): The input string to be encoded.
+            bos (bool): Whether to prepend the beginning-of-sequence token.
+            eos (bool): Whether to append the end-of-sequence token.
+
+        Returns:
+            List[int]: A list of token IDs.
+        """
+        assert type(s) is str
+        t = self.sp_model.encode(s)
+        if bos:
+            t = [self.bos_id] + t
+        if eos:
+            t = t + [self.eos_id]
+        return t
+
+    def decode(self, t: List[int]) -> str:
+        """
+        Decodes a list of token IDs into a string.
+
+        Args:
+            t (List[int]): The list of token IDs to be decoded.
+
+        Returns:
+            str: The decoded string.
+        """
+        return self.sp_model.decode(t)
---
+++ torchtrain/parallelisms/parallelize_llama.py
@@ -0,0 +1,238 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+# this file applies the PTD parallelisms and various training techniques to the
+# llama model, i.e. activation checkpointing, etc.
+
+from collections import defaultdict
+
+import torch
+from torch.distributed._tensor import Replicate, Shard
+
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
+    checkpoint_wrapper as ptd_checkpoint_wrapper,
+    CheckpointImpl,
+)
+from torch.distributed.fsdp import (
+    BackwardPrefetch,
+    FullyShardedDataParallel as FSDP,
+    MixedPrecision,
+    ShardingStrategy,
+)
+from torch.distributed.fsdp.wrap import enable_wrap, wrap
+from torch.distributed.tensor.parallel import (
+    ColwiseParallel,
+    parallelize_module,
+    PrepareModuleInput,
+    RowwiseParallel,
+    SequenceParallel,
+)
+
+from torch.utils.checkpoint import _pt2_selective_checkpoint_context_fn_gen, checkpoint
+
+from torchtrain.config_manager import JobConfig
+from torchtrain.logging_utils import logger
+from torchtrain.meta_init import meta_to_real_init_fn
+
+
+# for selective AC
+no_recompute_list = {
+    torch.ops.aten.mm.default,
+    torch.ops.aten._scaled_dot_product_efficient_attention.default,
+    torch.ops.aten._scaled_dot_product_flash_attention.default,
+    torch.ops.c10d_functional.reduce_scatter_tensor.default,
+}
+
+# Uses PTD FSDP AC wrapper
+# currently selective per op and per layer checkpointing are supported
+def checkpoint_wrapper(module, config):
+    if config.mode == "selective" and config.selective_ac_option == "op":
+
+        def _get_custom_policy(meta):
+            def _custom_policy(mode, func, *args, **kwargs):
+                mm_count_key = f"{mode}_mm_count"
+                if func == torch.ops.aten.mm.default:
+                    meta[mm_count_key] += 1
+                # Saves output of all compute ops, except every second mm
+                return func in no_recompute_list and not (
+                    func == torch.ops.aten.mm.default and meta[mm_count_key] % 2 == 0
+                )
+
+            return _custom_policy
+
+        def selective_checkpointing_context_fn():
+            meta = defaultdict(int)
+            return _pt2_selective_checkpoint_context_fn_gen(_get_custom_policy(meta))
+
+        return ptd_checkpoint_wrapper(
+            module,
+            checkpoint_impl=CheckpointImpl.NO_REENTRANT,
+            checkpoint_fn=checkpoint,
+            context_fn=selective_checkpointing_context_fn,
+            use_reentrant=False,
+            preserve_rng_state=False,
+        )
+    elif config.mode == "full":
+        # full AC
+        return ptd_checkpoint_wrapper(
+            module,
+            checkpoint_impl=CheckpointImpl.NO_REENTRANT,
+            checkpoint_fn=checkpoint,
+            use_reentrant=False,
+            preserve_rng_state=False,
+        )
+
+    elif config.mode == "selective" and config.selective_ac_option.isdigit():
+
+        """enables selective checkpointing of candidate layers.
+        Usage:
+        'selective_ac_option' with a positive 'int' value in config controls which layers to checkpoint.
+        1 == checkpointing every one (all).
+        2 == checkpoint every 2nd one
+        """
+        every_x_layer = int(config.selective_ac_option)
+        assert (
+            every_x_layer >= 0
+        ), f"selective layer AC policy (every_x_layer) expects a positive integer, received {every_x_layer}"
+
+        checkpoint_wrapper.__dict__.setdefault("_count", 0)
+
+        checkpoint_wrapper._count += 1
+        if not every_x_layer or checkpoint_wrapper._count % every_x_layer == 0:
+            return ptd_checkpoint_wrapper(
+                module,
+                checkpoint_impl=CheckpointImpl.NO_REENTRANT,
+                checkpoint_fn=checkpoint,
+                use_reentrant=False,
+                preserve_rng_state=False,
+            )
+        # skip activation checkpointing and store activations for this layer
+        else:
+            return module
+
+    else:
+        raise NotImplementedError(
+            "Unknown AC type or AC config. Only selective op and selective layer ac implemented currently."
+        )
+
+
+def parallelize_llama(model, world_mesh, parallel_dims, job_config: JobConfig):
+    """
+    Apply parallelisms to the model, including PTD parallelisms, and AC.
+
+    NOTE: the model passed in preferrablably shoule be a meta device model,
+    otherwise the model needs to be small enough on GPU or can fit into CPU.
+    """
+    # apply PTD parallelisms
+    if parallel_dims.pp_enabled:
+        raise NotImplementedError("PP not implemented yet.")
+
+    # First we apply Sequence Parallelism if it's enabled
+    if parallel_dims.sp_enabled:
+        tp_mesh = world_mesh["sp"]
+        sp_degree = job_config.training.sequence_parallel_degree
+
+        # First:
+        # 1. parallelize the first embedding and the last linear proj layer
+        # 2. shard the first layer of transformer block
+        model = parallelize_module(
+            model,
+            tp_mesh,
+            {
+                "embeddings.tok_embeddings": RowwiseParallel(
+                    input_layouts=Replicate(),
+                ),
+                "output": ColwiseParallel(
+                    input_layouts=Shard(0),
+                    output_layouts=Shard(-1)
+                    if parallel_dims.loss_parallel_enabled
+                    else Replicate(),
+                    use_local_output=not parallel_dims.loss_parallel_enabled,
+                ),
+                "norm": SequenceParallel(sequence_dim=0),
+                "layers.0": PrepareModuleInput(
+                    input_layouts=(Replicate(), None),
+                    desired_input_layouts=(Shard(0), None),
+                    use_local_output=True,
+                ),
+            },
+        )
+
+        # apply sequence parallelism to every transformer block
+        for layer_id, transformer_block in enumerate(model.layers):
+            layer_plan = {
+                "attention": PrepareModuleInput(
+                    input_layouts=(Shard(0), None),
+                    desired_input_layouts=(Replicate(), None),
+                ),
+                "attention.wq": ColwiseParallel(),
+                "attention.wk": ColwiseParallel(),
+                "attention.wv": ColwiseParallel(),
+                "attention.wo": RowwiseParallel(output_layouts=Shard(0)),
+                "attention_norm": SequenceParallel(sequence_dim=0),
+                "feed_forward": PrepareModuleInput(
+                    input_layouts=(Shard(0),),
+                    desired_input_layouts=(Replicate(),),
+                ),
+                "feed_forward.w1": ColwiseParallel(),
+                "feed_forward.w2": RowwiseParallel(output_layouts=Shard(0)),
+                "feed_forward.w3": ColwiseParallel(),
+                "ffn_norm": SequenceParallel(sequence_dim=0),
+            }
+
+            # adjust num_heads in attention layer to local heads
+            attn_layer = transformer_block.attention
+            attn_layer.n_heads = attn_layer.n_heads // sp_degree
+            attn_layer.n_kv_heads = attn_layer.n_kv_heads // sp_degree
+
+            parallelize_module(
+                module=transformer_block,
+                device_mesh=tp_mesh,
+                parallelize_plan=layer_plan,
+            )
+
+        logger.info("Applied Sequence Parallelism to the model")
+
+    if parallel_dims.dp_enabled:
+        dp_mesh = world_mesh["dp"]
+
+        fsdp_config = {
+            "mixed_precision": MixedPrecision(
+                param_dtype=torch.bfloat16,
+                # TODO: see whether we should expose a option to user
+                reduce_dtype=torch.float32,
+            ),
+            "sharding_strategy": ShardingStrategy.FULL_SHARD,
+            "backward_prefetch": BackwardPrefetch.BACKWARD_PRE,
+            # When torch.compile is active, it requires us to set use_orig_params=True
+            "use_orig_params": True,
+            "device_mesh": dp_mesh,
+            "param_init_fn": meta_to_real_init_fn,
+        }
+
+        with enable_wrap(wrapper_cls=FSDP, **fsdp_config):
+            for layer_id, transformer_block in enumerate(model.layers):
+                # apply AC to the transformer block
+                if job_config.activation_checkpoint.mode in ("full", "selective"):
+                    # wrap the transformer block with checkpoint wrapper, using config settings
+                    transformer_block = checkpoint_wrapper(
+                        transformer_block, job_config.activation_checkpoint
+                    )
+
+                # Wraps each layer with FSDP
+                model.layers[layer_id] = wrap(transformer_block)
+
+            # wrap the rest layers with FSDP
+            model = wrap(model)
+
+        logger.info("Applied FSDP to the model")
+    else:
+        meta_to_real_init_fn(model)
+        model.cuda()
+
+    # we have now moved from meta to device,
+    # reset parameters for proper initialization
+    model.reset_parameters()
+    logger.info("Model fully initialized via reset_parameters")
+
+    return model
