da4bd209686ea9d500b89fc210a789ea867e0f4c
lessw2020 <lessw@etrillium.com>
2024-04-04 21:12:53 -0700

    correct merge conflict in run.sh

--- run_llama_train.sh
+++ run_llama_train.sh
@@ -22,3 +22,33 @@ CONFIG_FILE=${CONFIG_FILE:-"./train_configs/debug_model.toml"}
 torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
 --local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
 train.py --job.config_file ${CONFIG_FILE}
+
+#!/usr/bin/bash
+
+set -ex
+
+# libUV is a scalable backend for TCPStore which is used in processGroup
+# rendezvous. This is the recommended backend for distributed training.
+export USE_LIBUV=1
+TRAINER_DIR=${1:-/home/$USER/local/torchtrain}
+
+# use envs as local overrides for convenience
+# e.g.
+# LOG_RANK=0,1 NGPU=4 ./run_llama_train.sh
+
+NGPU=${NGPU:-"8"}
+
+# by default log just rank 0 output,
+LOG_RANK=${LOG_RANK:-0}
+
+
+CONFIG_FILE=${CONFIG_FILE:-"./train_configs/debug_model.toml"}
+
+overrides=""
+if [ $# -ne 0 ]; then
+    overrides="$*"
+fi
+
+torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
+--local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
+train.py --job.config_file ${CONFIG_FILE} $overrides
