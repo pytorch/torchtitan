3ea0ff3a10161490d1c203d3e05c5258ff4eb1be
wz337 <wz337@cornell.edu>
2024-04-04 18:08:16 -0700

    support sequence of tests and add checkpoint test
    address comments
    
    [ghstack-poisoned]

--- test/test_runner.py
+++ test/test_runner.py
@@ -21,11 +21,12 @@ class OverrideDefinitions:
     This class is used to define the override definitions for the integration tests.
     """
 
-    override_args: Sequence[str] = tuple()
-    test_descr: str = "default"
+    override_args: Sequence[Sequence[str]] = tuple(tuple(" "))
+    test_descr: str = ""
 
 
 CONFIG_DIR = "./train_configs"
+test_checkpoint_dir = "./test_runner_checkpoint"
 
 """
 key is the config file name and value is a list of OverrideDefinitions
@@ -34,11 +35,45 @@ same root config file.
 """
 integration_tests_flavors = defaultdict(list)
 integration_tests_flavors["debug_model.toml"] = [
-    OverrideDefinitions(["--training.compile"], "1D compile"),
     OverrideDefinitions(
-        ["--training.tensor_parallel_degree 2"], "Eager mode 2DParallel"
+        [
+            ["--training.compile"],
+        ],
+        "1D compile",
+    ),
+    OverrideDefinitions(
+        [
+            ["--training.tensor_parallel_degree 2"],
+        ],
+        "Eager mode 2DParallel",
+    ),
+    OverrideDefinitions(
+        [
+            [f"--checkpoint.folder {test_checkpoint_dir}"],
+            [f"--checkpoint.folder {test_checkpoint_dir}", "--training.steps 20"],
+        ],
+        "Checkpoint Integration Test",
     ),
 ]
+
+
+def run_test(test_flavor: OverrideDefinitions, full_path: str):
+    # run_test supports sequence of tests.
+    for override_arg in test_flavor.override_args:
+        cmd = f"CONFIG_FILE={full_path} NGPU=4 ./run_llama_train.sh"
+        if override_arg:
+            cmd += " " + " ".join(override_arg)
+        print(
+            f"=====Integration test, flavor : {test_flavor.test_descr}, command : {cmd}====="
+        )
+        result = subprocess.run(
+            [cmd],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+            shell=True,
+        )
+        print(result.stdout)
 
 
 for config_file in os.listdir(CONFIG_DIR):
@@ -48,21 +83,7 @@ for config_file in os.listdir(CONFIG_DIR):
             config = tomllib.load(f)
             is_integration_test = config["job"].get("use_for_integration_test", False)
             if is_integration_test:
-                test_flavors = [OverrideDefinitions()] + integration_tests_flavors[
-                    config_file
-                ]
+                test_flavors = integration_tests_flavors[config_file]
+
                 for test_flavor in test_flavors:
-                    cmd = f"CONFIG_FILE={full_path} NGPU=4 ./run_llama_train.sh"
-                    if test_flavor.override_args:
-                        cmd += " " + " ".join(test_flavor.override_args)
-                    print(
-                        f"=====Integration test, flavor : {test_flavor.test_descr}, command : {cmd}====="
-                    )
-                    result = subprocess.run(
-                        [cmd],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.STDOUT,
-                        text=True,
-                        shell=True,
-                    )
-                    print(result.stdout)
+                    run_test(test_flavor, full_path)
