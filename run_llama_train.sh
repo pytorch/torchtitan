059a2ca8b8f0be4bc2e13bfc8c850a3f18ad156e
Will Constable <whc@meta.com>
2024-04-10 10:41:53 -0700

    Update (base update)
    [ghstack-poisoned]

--- run_llama_train.sh
+++ run_llama_train.sh
@@ -19,6 +19,11 @@ LOG_RANK=${LOG_RANK:-0}
 
 CONFIG_FILE=${CONFIG_FILE:-"./train_configs/debug_model.toml"}
 
-torchrun --nproc_per_node=${NGPU} --rdzv_endpoint="localhost:5972" \
+overrides=""
+if [ $# -ne 0 ]; then
+    overrides="$*"
+fi
+
+torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
 --local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
-train.py --job.config_file ${CONFIG_FILE}
+train.py --job.config_file ${CONFIG_FILE} $overrides
