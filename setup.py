7752631880838a1d06ad4199a1f50ae97e5362da
Wanchao Liang <wanchaol@users.noreply.github.com>
2024-04-11 13:34:16 -0700

    more lint

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
--- torchtitan/models/__init__.py
+++ torchtitan/models/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
 from torchtitan.models.llama import llama_configs, Transformer
 
 models_config = {
