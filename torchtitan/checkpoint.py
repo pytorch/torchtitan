cf1ef296cc7748b8da1029f19d8c3ff3c0ece037
Will Constable <whc@meta.com>
2024-04-12 17:07:26 -0700

    Update (base update)
    [ghstack-poisoned]

---
+++ torchtitan/checkpoint.py
@@ -0,0 +1,191 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+import enum
+import os
+import re
+import time
+from typing import Any, Dict
+
+import torch
+import torch.distributed as dist
+import torch.distributed.checkpoint as dcp
+import torch.nn as nn
+from torch.distributed.checkpoint.state_dict import (
+    get_model_state_dict,
+    get_optimizer_state_dict,
+    set_model_state_dict,
+    set_optimizer_state_dict,
+)
+from torchtitan.logging_utils import logger
+
+
+DTYPE_MAP = {
+    "float16": torch.float16,
+    "float32": torch.float32,
+    "bfloat16": torch.bfloat16,
+}
+
+
+class IntervalType(enum.Enum):
+    SECONDS = enum.auto()
+    STEPS = enum.auto()
+
+
+class ModelWrapper:
+    def __init__(self, model: nn.Module) -> None:
+        self.model = model
+
+    def state_dict(self) -> None:
+        return get_model_state_dict(self.model)
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        set_model_state_dict(self.model, state_dict)
+
+
+class OptimizerWrapper:
+    def __init__(self, model: nn.Module, optim: torch.optim.Optimizer) -> None:
+        self.model = model
+        self.optim = optim
+
+    def state_dict(self) -> None:
+        return get_optimizer_state_dict(self.model, self.optim)
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        set_optimizer_state_dict(self.model, self.optim, optim_state_dict=state_dict)
+
+
+class CheckpointManager:
+    def __init__(
+        self,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        states: Dict[str, Any],
+        folder: str,
+        interval_type: IntervalType,
+        interval: int,
+        model_weights_only: bool = False,
+        export_dtype: str = "float32",
+    ) -> None:
+        self.folder = folder
+        self.states = states
+        self.model_weights_only = model_weights_only
+        self.states.update(
+            {
+                "model": ModelWrapper(model),
+                "optimizer": OptimizerWrapper(model, optimizer),
+            }
+        )
+        self.interval_type = interval_type
+        self.interval = interval
+        self.begin = 0
+        self.work = None
+        self.pg = dist.new_group(backend="gloo")
+        self.doit = None
+        self.export_dtype = DTYPE_MAP[export_dtype]
+
+        if self.folder:
+            logger.info(
+                f"Checkpointing active. Checkpoints will be loaded from and saved to {self.folder}"
+            )
+
+    def reset(self) -> None:
+        self.begin = time.monotonic()
+
+    def create_checkpoint_id(self, step: int) -> str:
+        return os.path.join(self.folder, f"step-{step}")
+
+    def save(self, curr_step: int, force: bool = False) -> None:
+        """
+        force = True will force the checkpoint to be saved, even if the interval has not been reached.
+        This only happens when train_state.step == job_config.training.steps.
+        """
+        if not self.folder:
+            return
+
+        if not force:
+            if self.interval_type == IntervalType.STEPS and not (
+                curr_step % self.interval == 0
+            ):
+                return
+            if self.interval_type == IntervalType.SECONDS:
+                doit = (time.monotonic() - self.begin) >= self.interval
+                self.doit = torch.tensor(int(doit))
+                if self.work is None:
+                    self.work = dist.all_reduce(self.doit, group=self.pg, async_op=True)
+                    return
+                elif curr_step % 5 == 4:
+                    self.work.wait()
+                    self.work = None
+                    doit = self.doit.item()
+                    self.doit = None
+                    if doit == 0:
+                        return
+                else:
+                    return
+
+        if self.work:
+            self.work.wait()
+            self.work = None
+            self.doit = None
+
+        # We only consider saving weights only at the end of the training.
+        # So this won't affect preemption and training resume.
+        # We also only allow dtype conversion when we are checkpoint model weights only
+        # and the current dtype is not the same as the export dtype at the end of the training.
+        if force and self.model_weights_only:
+            # We update self.states to keep the model only.
+            # After this update, self.states = {'tok_embeddings.weight':...,''layers.0.attention.wq.weight': ...}.
+            self.states = self.states["model"].state_dict()
+
+            # For now, we will manually pop the freqs_cis buffer, as we made this permanent
+            # temporarily and we don't want to include it in the exported state_dict.
+            # Context: https://github.com/pytorch/torchtitan/blob/main/torchtitan/models/llama/model.py#L348
+            self.states.pop("freqs_cis")
+
+            if self.export_dtype != torch.float32:
+                self.states = {
+                    k: v.to(self.export_dtype) for k, v in self.states.items()
+                }
+            logger.info(
+                f"Saving a model weights only checkpoint in {self.export_dtype} at step {curr_step}"
+            )
+
+        else:
+            logger.info(f"Saving a full checkpoint at step {curr_step}")
+
+        begin = time.monotonic()
+        dcp.save(self.states, checkpoint_id=self.create_checkpoint_id(curr_step))
+        self.reset()
+        logger.info(
+            f"Finished saving the checkpoint in {time.monotonic() - begin:.2f} seconds"
+        )
+
+    def load(self, step: int = -1) -> bool:
+        if not self.folder:
+            return False
+        if not os.path.isdir(self.folder):
+            return False
+        if step != -1 and not os.path.isdir(self.create_checkpoint_id(step)):
+            return False
+
+        if step == -1:
+            step_counts = []
+            for filename in os.listdir(self.folder):
+                match = re.search(r"step-(\d+)", filename)
+                if match:
+                    step_counts.append(int(match.group(1)))
+            if not step_counts:
+                return False
+            step = max(step_counts)
+
+        logger.info(f"Loading the checkpoint at step {step}")
+        begin = time.monotonic()
+        dcp.load(
+            self.states,
+            checkpoint_id=self.create_checkpoint_id(step),
+        )
+        logger.info(
+            f"Finished loading the checkpoint in {time.monotonic() - begin:.2f} seconds"
+        )
+        return True
---
+++ torchtitan/datasets/hf_datasets.py
@@ -0,0 +1,168 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+from typing import List, Optional
+
+import torch
+from torch.utils.data import DataLoader, IterableDataset
+
+from torchtitan.datasets.tokenizer import TokenizerIf
+from torchtitan.logging_utils import logger
+
+from datasets import load_dataset, load_from_disk
+from datasets.distributed import split_dataset_by_node
+
+_supported_datasets = {
+    "alpaca": "tatsu-lab/alpaca",
+    "minipile": "JeanKaddour/minipile",
+    "c4": "allenai/c4",
+    "openwebtext": "Skylion007/openwebtext",
+}
+
+
+class HuggingFaceDataset(IterableDataset):
+    """PyTorch Representation of the HuggingFace Dataset.
+
+    Args:
+        dataset_name (str): name of the dataset to load
+        dataset_path (Optional[str]): Path to the dataset in the file system. If provided, data will be loaded from this path instead of downloaded.
+        tokenizer (TokenizerIf): Tokenizer used to encode data. Tokenize must implement an `encode` and `decode` method.
+        seq_len (int): max sequence length
+        world_size (int): number of data parallel processes participating in training
+        rank (int): rank of the current data parallel process
+        infinite (bool): whether to loop infinitely over the dataset
+
+    We currently support four datasets:
+    alpaca (52K training entries)
+    minipile (1M training entries, amalgamated from other datasets)
+    openwebtext (1M training entries, same type of data for entire dataset)
+    c4 (177M training entries - this dataset is streamed due to the size)
+
+    >> Alpaca <<:
+    Data input format (alpaca):
+    {
+        "instruction": "Create a classification task by clustering the given list of items.",
+        "input": "Apples, oranges, bananas, strawberries, pineapples",
+        "output": "Class 1: Apples, Oranges\nClass 2: Bananas, Strawberries\nClass 3: Pineapples",
+        "text": "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\nCreate a classification task by clustering the given list of items.\n\n### Input:\nApples, oranges, bananas, strawberries, pineapples\n\n### Response:\nClass 1: Apples,
+        Oranges\nClass 2: Bananas, Strawberries\nClass 3: Pineapples",  # noqa: B950
+    }
+
+    >> MiniPile <<:
+    MiniPile dataset is detailed in the paper: https://arxiv.org/abs/2304.08442
+    Data input format (minipile):
+    {
+        "text": "Open-end spinning devices with such rotor bearing arrangements are known in
+                various different embodiments, and have been extensively described,
+                for example in German Patent Publications"
+    }
+
+     >> c4 (EN) <<:
+    c4 cleaned, English version
+    Data input format (c4):
+    {
+    'url': 'https://klyq.com/beginners-bbq-class-taking-place-in-missoula/',
+    'text': 'Beginners BBQ Class Taking Place in Missoula!\nDo you want to get better at making delicious BBQ? You will have the opportunity, put this on your calendar now. Thursday, September 22nd join World Class BBQ Champion, Tony Balay from Lonestar Smoke Rangers. He will be teaching a beginner level class for everyone who wants to get better with their culinary skills.\nHe will teach you everything you need to know to compete in a KCBS BBQ competition, including techniques, recipes, timelines, meat selection and trimming, plus smoker and fire information.\nThe cost to be in the class is $35 per person, and for spectators it is free. Included in the cost will be either a t-shirt or apron and you will be tasting samples of each meat that is prepared.',
+    'timestamp': '2019-04-25T12:57:54Z'
+    }
+
+    >> OpenWebText <<:
+    OpenWeb crawl, English
+    Example:
+    {
+        'text': "Amazon has launched a new cheaper version of its Echo Dot voice-controlled device today.
+    The launch comes six months after Amazon first introduced two new Echo devices —
+    one of which was the $90 Echo Dot,..."
+    }
+
+    Example use (alpaca):
+    >>> alpaca_ds = HuggingFaceDataset(dataset_name="alpaca", dataset_path=None, tokenizer=tokenizer)
+    >>> for batch in Dataloader(alpaca_ds, batch_size=8):
+            print(f"Batch size: {len(batch)}")
+        Batch size: 8
+
+
+    """
+
+    def __init__(
+        self,
+        dataset_name: str,
+        dataset_path: Optional[str],
+        tokenizer: TokenizerIf,
+        seq_len: int = 2048,
+        world_size: int = 1,
+        rank: int = 0,
+        infinite: bool = False,
+    ) -> None:
+        if dataset_name not in _supported_datasets:
+            raise ValueError(
+                f"Dataset {dataset_name} is not supported. Supported datasets are: {_supported_datasets.keys()}."
+            )
+
+        # TODO: This is a temporary solution for small datasets like Alpaca.
+        #       For larger datasets we need to use a more scalable approach.
+        if dataset_path:
+            logger.info(f"Loading {dataset_name} dataset locally from {dataset_path}")
+            ds = load_from_disk(dataset_path)
+        else:
+            logger.info(f"Preparing {dataset_name} dataset from HuggingFace")
+            # Setting `streaming=True` works for large dataset, but is slightly slower and unstable.
+            # c4 is huge, and requires both streaming and language selection (we default to en)
+            if dataset_name == "c4":
+                ds = load_dataset(
+                    _supported_datasets[dataset_name],
+                    "en",
+                    split="train",
+                    streaming=True,
+                )
+            else:
+                ds = load_dataset(_supported_datasets[dataset_name], split="train")
+
+        self.dataset_name = dataset_name
+        self._data = split_dataset_by_node(ds, rank, world_size)
+        self._tokenizer = tokenizer
+        self.seq_len = seq_len
+        self.infinite = infinite
+
+    def __iter__(self):
+        max_buffer_token_len = 1 + self.seq_len
+        all_tokens: List[int] = []
+
+        while True:
+            for sample in iter(self._data):
+                sample_text = sample["text"]
+                sample_tokens = self._tokenizer.encode(sample_text, bos=True, eos=True)
+                all_tokens.extend(sample_tokens)
+
+                while len(all_tokens) >= max_buffer_token_len:
+                    x = torch.LongTensor(all_tokens[:max_buffer_token_len])
+                    # update tokens to the remaining tokens
+                    all_tokens = all_tokens[max_buffer_token_len:]
+                    input = x[:-1]
+                    label = x[1:]
+                    yield input, label
+            if not self.infinite:
+                logger.warning(f"Dataset {self.dataset_name} has run out of data.")
+                break
+            else:
+                logger.warning(
+                    f"Dataset {self.dataset_name} is being re-looped. "
+                    "Loss related metrics might be misleading."
+                )
+
+
+def build_hf_data_loader(
+    dataset_name: str,
+    dataset_path: Optional[str],
+    tokenizer: TokenizerIf,
+    batch_size: int,
+    seq_len: int,
+    world_size,
+    rank,
+    infinite: bool = True,
+):
+    hf_ds = HuggingFaceDataset(
+        dataset_name, dataset_path, tokenizer, seq_len, world_size, rank, infinite
+    )
+
+    return DataLoader(hf_ds, batch_size=batch_size)
