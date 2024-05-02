c4c0e1e4711843dce72d108d55d9b4fd8568c102
Will Constable <whc@meta.com>
2024-05-01 22:08:28 -0700

    Update (base update)
    [ghstack-poisoned]

--- run_llama_train.sh
+++ run_llama_train.sh
@@ -31,4 +31,4 @@ fi
 
 torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
 --local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
-train.py --job.config_file ${CONFIG_FILE} $overrides --training.checkpoint_folder /data/users/whc/torchtrain
+train.py --job.config_file ${CONFIG_FILE} $overrides
