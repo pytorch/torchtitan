3d7973a0e58015c751165f639f906a444b6c5b1e
Will Constable <whc@meta.com>
2024-05-10 18:20:02 -0700

    Make Transformer tolerate missing layers for PP
    A few small changes here lets manual PP frontend 'reconfigure' a whole
    transformer model to a stage's portion simply by setting undesired
    layers to None (in cases of top level layers) or deleting them from the
    ModuleDict (for 'layers.*').
    
    These changes don't impact the FQNs of the remaining layers, which is
    critical for checkpoint load/save compatibility.
    
    ghstack-source-id: 48a7aafc89d86c3168f905560a4cd6bf4b5b9a12
    Pull Request resolved: https://github.com/pytorch/torchtitan/pull/322

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -366,9 +366,9 @@ class Transformer(nn.Module):
         # just the non-persistent buffers that is called after loading checkpoints.
         self.register_buffer("freqs_cis", self._precompute_freqs_cis(), persistent=True)
 
-        self.layers = torch.nn.ModuleList()
+        self.layers = torch.nn.ModuleDict()
         for layer_id in range(model_args.n_layers):
-            self.layers.append(TransformerBlock(layer_id, model_args))
+            self.layers[str(layer_id)] = TransformerBlock(layer_id, model_args)
 
         self.norm = create_norm(
             model_args.norm_type, dim=model_args.dim, eps=model_args.norm_eps
@@ -392,7 +392,7 @@ class Transformer(nn.Module):
         with torch.device(self.freqs_cis.device):
             self.freqs_cis = self._precompute_freqs_cis()
         nn.init.normal_(self.tok_embeddings.weight)
-        for layer in self.layers:
+        for layer in self.layers.values():
             layer.init_weights()
         self.norm.reset_parameters()
         final_out_std = self.model_args.dim**-0.5
@@ -425,12 +425,14 @@ class Transformer(nn.Module):
             torch.Tensor: Output logits after applying the Transformer model.
 
         """
-        h = self.tok_embeddings(tokens)
+        # passthrough for nonexistent layers, allows easy configuration of pipeline parallel stages
+        h = self.tok_embeddings(tokens) if self.tok_embeddings else tokens
 
-        for layer in self.layers:
+        for layer in self.layers.values():
             h = layer(h, self.freqs_cis)
-        h = self.norm(h)
-        output = self.output(h).float()
+
+        h = self.norm(h) if self.norm else h
+        output = self.output(h).float() if self.output else h
         return output
 
     @classmethod
