55171e4355da31d9134107f2448680825709e5e5
Will Constable <whc@meta.com>
2024-05-14 20:08:03 -0700

    Update (base update)
    [ghstack-poisoned]

---
+++ torchtitan/profiling.py
@@ -0,0 +1,72 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import contextlib
+import os
+import time
+
+import torch
+from torchtitan.config_manager import JobConfig
+from torchtitan.logging_utils import logger
+
+# the number of warmup steps before the active step in each profiling cycle
+WARMUP = 3
+
+
+@contextlib.contextmanager
+def maybe_enable_profiling(config: JobConfig, *, global_step: int = 0):
+    # get user defined profiler settings
+    enable_profiling = config.profiling.enable_profiling
+
+    if enable_profiling:
+        dump_dir = config.job.dump_folder
+        save_trace_dir = config.profiling.save_traces_folder
+        trace_dir = os.path.join(dump_dir, save_trace_dir)
+        profile_freq = config.profiling.profile_freq
+
+        rank = torch.distributed.get_rank()
+
+        def trace_handler(prof):
+            curr_trace_dir_name = "iteration_" + str(prof.step_num)
+            curr_trace_dir = os.path.join(trace_dir, curr_trace_dir_name)
+            if not os.path.exists(curr_trace_dir):
+                os.makedirs(curr_trace_dir, exist_ok=True)
+
+            logger.info(f"Dumping traces at step {prof.step_num}")
+            begin = time.monotonic()
+            prof.export_chrome_trace(f"{curr_trace_dir}/rank{rank}_trace.json")
+            logger.info(
+                f"Finished dumping traces in {time.monotonic() - begin:.2f} seconds"
+            )
+            # Profiling is a heavy operation which could cost very different amount of time
+            # across all ranks. Insert a barrier to make sure all ranks have finished profiling
+            # before moving on.
+            # TODO: Can we find a cleaner way?
+            torch.distributed.barrier()
+
+        logger.info(f"Profiling active. Traces will be saved at {trace_dir}")
+
+        if not os.path.exists(trace_dir):
+            os.makedirs(trace_dir, exist_ok=True)
+
+        warmup, active = WARMUP, 1
+        wait = profile_freq - (active + warmup)
+        assert (
+            wait >= 0
+        ), "profile_freq must be greater than or equal to warmup + active"
+        with torch.profiler.profile(
+            activities=[
+                torch.profiler.ProfilerActivity.CPU,
+                torch.profiler.ProfilerActivity.CUDA,
+            ],
+            schedule=torch.profiler.schedule(wait=wait, warmup=warmup, active=active),
+            on_trace_ready=trace_handler,
+        ) as torch_profiler:
+            torch_profiler.step_num = global_step
+            yield torch_profiler
+    else:
+        torch_profiler = contextlib.nullcontext()
+        yield None
