3e3b3d8e111dca44679c50cd437444215f09d888
Liang Luo <liangluo@fb.com>
2024-05-27 01:23:09 -0700

    update test

--- test/test_job_config.py
+++ test/test_job_config.py
@@ -19,7 +19,7 @@ class TestJobConfig:
     def test_job_config_file(self):
         config = JobConfig()
         config.parse_args(["--job.config_file", "./train_configs/debug_model.toml"])
-        assert config.training.steps == 10
+        assert config.training.steps == 100
 
     def test_job_file_does_not_exist(self):
         with pytest.raises(FileNotFoundError):
