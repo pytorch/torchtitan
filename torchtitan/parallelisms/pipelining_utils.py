31956c7f944318edb8bd28c7f81ecf5347694e5f
Will Constable <whc@meta.com>
2024-06-20 14:06:13 -0700

    Update
    [ghstack-poisoned]

--- torchtitan/parallelisms/pipelining_utils.py
+++ torchtitan/parallelisms/pipelining_utils.py
@@ -37,3 +37,24 @@ def build_pipeline_schedule(job_config, parallel_dims, stages, loss_fn):
         n_microbatches=n_microbatches,
         loss_fn=loss_fn,
     )
+
+
+# TODO(whc) should this be a utility inside torch.pipelining?
+def stage_ids_this_rank(
+    rank: int, pp_size: int, num_stages: int, style: str = "loop"
+) -> Tuple[int]:
+    """Compute the stage ids for the stages that will run on this pp rank for either a looped or V style schedule"""
+    assert (
+        num_stages % pp_size == 0
+    ), f"num_stages {num_stages} must be evenly divisible by pp_size {pp_size}"
+    stages_per_rank = num_stages // pp_size
+    if style == "loop":
+        return tuple(pp_rank + s * pp_size for s in range(stages_per_rank))
+    elif style == "v":
+        assert (
+            stages_per_rank == 2
+        ), f"v schedules assume 2 stages per rank, got {stages_per_rank}"
+        stage_v_pairs = list(
+            zip(range(pp_size), range(num_stages - 1, pp_size - 1, -1))
+        )
+        return stage_v_pairs[pp_rank]
