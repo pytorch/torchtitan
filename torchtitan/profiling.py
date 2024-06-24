b1340a1380528f8a1b760c0cc21b4fe1ee56ca03
Andrew Gu <andgu@fb.com>
2024-06-24 13:44:35 -0700

    Set `record_shapes=True` for profiler
    ghstack-source-id: 6f1ed49d15ce311f1bf118820965cdb5309a8030
    Pull Request resolved: https://github.com/pytorch/torchtrain/pull/419

--- torchtitan/profiling.py
+++ torchtitan/profiling.py
@@ -68,6 +68,7 @@ def maybe_enable_profiling(config: JobConfig, *, global_step: int = 0):
             ],
             schedule=torch.profiler.schedule(wait=wait, warmup=warmup, active=active),
             on_trace_ready=trace_handler,
+            record_shapes=True,
         ) as torch_profiler:
             torch_profiler.step_num = global_step
             yield torch_profiler
