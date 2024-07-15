ac72078bb590d71a74872e514be01c579544ef84
Will Constable <whc@meta.com>
2024-07-14 21:17:03 -0700

    [Cleanup] Organize run_llama_train.sh options
    Just a little code motion but it looks cleaner to me this way
    
    ghstack-source-id: 055fbd557cd9cf189e6b9bd6a7048f1204e1dc5c
    Pull Request resolved: https://github.com/pytorch/torchtitan/pull/454

--- run_llama_train.sh
+++ run_llama_train.sh
@@ -7,19 +7,13 @@
 
 set -ex
 
-TRAINER_DIR=${TRAINER_DIR:-/home/$USER/local/torchtitan}
-
 # use envs as local overrides for convenience
 # e.g.
 # LOG_RANK=0,1 NGPU=4 ./run_llama_train.sh
-
 NGPU=${NGPU:-"8"}
 NNODES=${NNODES:-"1"}
-
-# by default log just rank 0 output,
+TRAINER_DIR=${TRAINER_DIR:-/home/$USER/local/torchtitan}
 LOG_RANK=${LOG_RANK:-0}
-
-
 CONFIG_FILE=${CONFIG_FILE:-"./train_configs/debug_model.toml"}
 
 overrides=""
