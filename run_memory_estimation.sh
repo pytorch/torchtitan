4b6cdc17627deae58113032889d8d04245a94111
Will Constable <whc@meta.com>
2024-07-14 21:17:03 -0700

    [Cleanup] Split run_llama_train.sh and run_memory_estimation.sh
    Make each script simpler to read
    
    ghstack-source-id: ba3aa65feb6e304736c73daf5bc8ab5fb254f196
    Pull Request resolved: https://github.com/pytorch/torchtitan/pull/455

--- run_memory_estimation.sh
+++ run_memory_estimation.sh
@@ -9,10 +9,10 @@ set -ex
 
 # use envs as local overrides for convenience
 # e.g.
-# LOG_RANK=0,1 NGPU=4 ./run_llama_train.sh
+# NGPU=4 ./run_memory_estimation.sh
 NGPU=${NGPU:-"8"}
+NNODES=${NNODES:-"1"}
 TRAINER_DIR=${TRAINER_DIR:-/home/$USER/local/torchtitan}
-LOG_RANK=${LOG_RANK:-0}
 CONFIG_FILE=${CONFIG_FILE:-"./train_configs/debug_model.toml"}
 
 overrides=""
@@ -20,6 +20,8 @@ if [ $# -ne 0 ]; then
     overrides="$*"
 fi
 
-torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
---local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
-train.py --job.config_file ${CONFIG_FILE} $overrides
+# Calculate WORLD_SIZE as the product of NGPU and NNODES
+# Export WORLD_SIZE and LOCAL_RANK
+export WORLD_SIZE=$((NGPU * NNODES))
+export LOCAL_RANK=0
+python estimation.py --job.config_file ${CONFIG_FILE} --memory_estimation.enabled $overrides
