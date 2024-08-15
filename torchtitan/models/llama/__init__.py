4fe231e3b2f37aad61780a07c65da0ace584ad82
Howard Huang <howardhuang@fb.com>
2024-08-15 14:07:36 -0700

    [WIP] zero bubble
    ghstack-source-id: 0937188a60a0c376ca03a34297ad885f7d29f05a
    Pull Request resolved: https://github.com/pytorch/torchtitan/pull/473

--- torchtitan/models/llama/__init__.py
+++ torchtitan/models/llama/__init__.py
@@ -32,8 +32,8 @@ llama3_configs = {
     "debugmodel": ModelArgs(dim=256, n_layers=8, n_heads=16, rope_theta=500000),
     "8B": ModelArgs(
         dim=4096,
-        n_layers=32,
-        n_heads=32,
+        n_layers=8,  # updated for PP
+        n_heads=16,
         n_kv_heads=8,
         ffn_dim_multiplier=1.3,
         multiple_of=1024,
