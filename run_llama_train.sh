be32d1987ea0a8ff33a1a48fa3442edc92148a3f
Andrew Gu <andgu@fb.com>
2024-10-22 08:13:02 -0700

    Use expandable segments in `run_llama_train.sh`
    [ghstack-poisoned]

--- run_llama_train.sh
+++ run_llama_train.sh
@@ -19,6 +19,7 @@ if [ $# -ne 0 ]; then
     overrides="$*"
 fi
 
+PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" \
 torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
 --local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
 train.py --job.config_file ${CONFIG_FILE} $overrides
