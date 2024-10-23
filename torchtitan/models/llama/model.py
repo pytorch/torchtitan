30c204f4fa0f94e758561ac5bc2cbff74ea16f42
mori360 <yifanmao@meta.com>
2024-10-23 14:10:34 -0700

    init freqs_cis at meta device

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -367,10 +367,9 @@ class Transformer(nn.Module):
         # initialized by the checkpoint, or we need to add a separate initializer for
         # just the non-persistent buffers that is called after loading checkpoints.
         self.register_buffer("freqs_cis", self._precompute_freqs_cis(), persistent=True)
-        with torch.device("cpu"):
-            self.freqs_cis = nn.Parameter(
-                self._precompute_freqs_cis().to(torch.float32), requires_grad=False
-            )
+        self.freqs_cis = nn.Parameter(
+            self._precompute_freqs_cis().to(torch.float32), requires_grad=False
+        )
         self.layers = torch.nn.ModuleDict()
         for layer_id in range(model_args.n_layers):
             self.layers[str(layer_id)] = TransformerBlock(layer_id, model_args)
@@ -396,6 +395,8 @@ class Transformer(nn.Module):
         """
         if self.tok_embeddings is not None:
             nn.init.normal_(self.tok_embeddings.weight)
+        if self.freqs_cis is not None:
+            nn.init.normal_(self.freqs_cis)
         for layer in self.layers.values():
             if layer is not None:
                 layer.init_weights()
