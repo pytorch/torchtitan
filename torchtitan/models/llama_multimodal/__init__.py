ccfc02be8871f395c30b91690ba8e0c5d36cb540
Junjie Wang <6937752+fduwjj@users.noreply.github.com>
2024-10-24 20:34:31 -0700

    [Multimodal model] Add the decoder of the multimodal model (#626)
    After https://github.com/pytorch/torchtitan/pull/589, we are adding
    decoder model to torchtitan with simple unit test.

--- torchtitan/models/llama_multimodal/__init__.py
+++ torchtitan/models/llama_multimodal/__init__.py
@@ -4,12 +4,16 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 #
-# Llama 2 is licensed under the LLAMA 2 Community License,
+# Llama 3 is licensed under the LLAMA 3 Community License,
 # Copyright (c) Meta Platforms, Inc. All Rights Reserved.
 
-from torchtitan.models.llama_multimodal.model import ModelArgs, VisionEncoder
+from torchtitan.models.llama_multimodal.model import (
+    ModelArgs,
+    MultimodalDecoder,
+    VisionEncoder,
+)
 
-__all__ = ["VisionEncoder", "ModelArgs"]
+__all__ = ["VisionEncoder", "ModelArgs", "MultimodalDecoder"]
 
 llama3_2_configs = {
     # TODO: add configs for llama3.2
