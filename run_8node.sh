2683b2c87613bdea367469470e5ee656c62c3fbe
zigzagcai <caizheng@pjlab.org.cn>
2024-11-12 10:49:02 +0800

    update config

--- run_8node.sh
+++ run_8node.sh
@@ -1,3 +1,3 @@
-srun -p llm_s -n 16 -N 16 --ntasks-per-node=1 --gpus-per-task=8 -w "HOST-10-140-60-[1-14,16,188]" \
+srun -p llm_s -n 8 -N 8 --ntasks-per-node=1 --gpus-per-task=8 -w "HOST-10-140-60-[1-8]" \
 torchrun --nproc_per_node 8 --rdzv_backend c10d --rdzv_id 101 --rdzv_endpoint "10.140.60.12:29500" \
 --local-ranks-filter 0 ./train.py --job.config_file ./train_configs/llama2_70b.toml
