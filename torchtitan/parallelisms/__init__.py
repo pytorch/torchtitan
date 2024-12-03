4d88a8a44fe439f73f564b83d60b29a906ef3507
Mark Saroufim <marksaroufim@meta.com>
2024-12-03 13:38:59 -0800

    [BE]: Deprecate Llama2 (#712)
    Deprecates Llama2 utilities and mentions in the documentation. Also
    deprecated sentencepiece since that's only used for llama2. Code could
    be simplified further but from a code quality perspective it might make
    sense to have one more large model variant in the repo.
    
    ---------
    
    Co-authored-by: tianyu-l <150487191+tianyu-l@users.noreply.github.com>

--- torchtitan/parallelisms/__init__.py
+++ torchtitan/parallelisms/__init__.py
@@ -17,10 +17,8 @@ __all__ = [
 ]
 
 models_parallelize_fns = {
-    "llama2": parallelize_llama,
     "llama3": parallelize_llama,
 }
 models_pipelining_fns = {
-    "llama2": pipeline_llama,
     "llama3": pipeline_llama,
 }
