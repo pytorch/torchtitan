bc5f13714f62e184c51b60afce5f3884f6fe36f9
Xilun Wu <12968408+XilunWu@users.noreply.github.com>
2025-01-16 16:47:42 -0800

    Update on "[do NOT land] CP+torch.compile debugging attempt"
    [ghstack-poisoned]

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -173,6 +173,10 @@ class Attention(nn.Module):
     def SDPA(self, *args, **kwargs):
         return F.scaled_dot_product_attention(*args, **kwargs)
 
+    @torch.compiler.disable
+    def noop(self):
+        return None
+
     def forward(
         self,
         x: torch.Tensor,
@@ -209,9 +213,11 @@ class Attention(nn.Module):
         xk = keys.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
         xv = values.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
 
-        # we use casual mask for training
-        # output = F.scaled_dot_product_attention(xq, xk, xv, is_causal=True)
-        output = self.SDPA(xq, xk, xv, is_causal=True)
+        self.noop()
+        # we use casual mask for training                
+        output = F.scaled_dot_product_attention(xq, xk, xv, is_causal=True)
+        # output = self.SDPA(xq, xk, xv, is_causal=True)
+        self.noop()
         output = output.transpose(
             1, 2
         ).contiguous()  # (bs, seqlen, n_local_heads, head_dim)
