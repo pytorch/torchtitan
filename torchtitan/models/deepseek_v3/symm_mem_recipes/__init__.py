502a1e56d413135c808513df17e6331b2f46690e
Ke Wen <kw2501@meta.com>
2025-02-20 15:51:19 -0800

    Add on_device_all_to_all_v

---
+++ torchtitan/models/deepseek_v3/symm_mem_recipes/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .triton_on_device_all_to_all_v import on_device_all_to_all_v
+
+__all__ = [
+    "on_device_all_to_all_v",
+]
