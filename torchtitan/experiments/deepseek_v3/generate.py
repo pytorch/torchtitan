68b1b992d17e3d4125843456e2a818e8f9a2b5fd
Eugen Hotaj <eugen@perplexity.ai>
2025-03-14 04:15:12 +0000

    cleanup

--- torchtitan/experiments/deepseek_v3/generate.py
+++ torchtitan/experiments/deepseek_v3/generate.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# torchrun --standalone --nproc-per-node 4 run.py
+# torchrun --standalone --nproc-per-node 4 generate.py
 
 from dataclasses import dataclass
 
