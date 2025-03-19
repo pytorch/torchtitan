d050a76642661977cb14f959fdc902524e34d0fb
Jiani Wang <jianiw@meta.com>
2025-03-19 14:00:07 -0700

    fix llama_multimodal model

--- torchtitan/models/llama_multimodal/model.py
+++ torchtitan/models/llama_multimodal/model.py
@@ -1136,8 +1136,8 @@ class CrossAttention(nn.Module):
             model_args.decoder_embed_dim,
             bias=False,
         )
-        self.q_norm = nn.RMSNorm(dim=self.head_dim, eps=1e-05)
-        self.k_norm = nn.RMSNorm(dim=self.head_dim, eps=1e-05)
+        self.q_norm = nn.RMSNorm(self.head_dim, eps=1e-05)
+        self.k_norm = nn.RMSNorm(self.head_dim, eps=1e-05)
 
     def init_weights(self, init_std: float):
         for linear in (self.wq, self.wk, self.wv):
@@ -1190,14 +1190,14 @@ class DecoderTransformerSelfAttnBlock(nn.Module):
     ):
         super().__init__()
         self.attn = SelfAttention(model_args)
-        self.ln_attn = nn.RMSNorm(dim=model_args.decoder_embed_dim, eps=1e-5)
+        self.ln_attn = nn.RMSNorm(model_args.decoder_embed_dim, eps=1e-5)
         self.mlp = FeedForwardForDecoder(
             dim=model_args.decoder_embed_dim,
             hidden_dim=4 * model_args.decoder_embed_dim,
             multiple_of=model_args.multiple_of,
             ffn_dim_multiplier=model_args.ffn_dim_multiplier,
         )
-        self.ln_mlp = nn.RMSNorm(dim=model_args.decoder_embed_dim, eps=1e-5)
+        self.ln_mlp = nn.RMSNorm(model_args.decoder_embed_dim, eps=1e-5)
 
     def forward(
         self,
@@ -1218,14 +1218,14 @@ class DecoderTransformerCrossAttnBlock(nn.Module):
     ):
         super().__init__()
         self.attn = CrossAttention(model_args)
-        self.ln_attn = nn.RMSNorm(dim=model_args.decoder_embed_dim)
+        self.ln_attn = nn.RMSNorm(model_args.decoder_embed_dim)
         self.mlp = FeedForward(
             dim=model_args.decoder_embed_dim,
             hidden_dim=4 * model_args.decoder_embed_dim,
             multiple_of=model_args.multiple_of,
             ffn_dim_multiplier=model_args.ffn_dim_multiplier,
         )
-        self.ln_mlp = nn.RMSNorm(dim=model_args.decoder_embed_dim)
+        self.ln_mlp = nn.RMSNorm(model_args.decoder_embed_dim)
         self.attn_scale = TanhGate()
         self.mlp_scale = TanhGate()
 
