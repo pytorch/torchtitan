95b9e78ab7c0307b2ed0f29ca8b1d26ecf83b79f
Ke Wen <kw2501@meta.com>
2025-03-19 20:06:12 -0700

    [DeepSeek] Remove support of EP=1 case
    ghstack-source-id: 1a4b8e362eb5b51d0637794eb64ce95de25c11b0
    Pull Request resolved: https://github.com/pytorch/torchtitan/pull/954

--- torchtitan/experiments/deepseek_v3/model_config.py
+++ torchtitan/experiments/deepseek_v3/model_config.py
@@ -142,6 +142,7 @@ class ModelArgs:
     pad_token_id = None
     # Added for symmetric memory
     max_seq_len: int = 4096
+    dtype: str = "bfloat16"
     # Added for pipeline parallel
     num_stages: int = 1
     stage_idx: int = 0
