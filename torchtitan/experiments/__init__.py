98f7a12b16918bd2cdd70df704ad9d557dd4927e
Tianyu Liu <lty@fb.com>
2025-03-23 18:33:37 -0700

    move model-specific args logic to its ModelArgs

--- torchtitan/experiments/__init__.py
+++ torchtitan/experiments/__init__.py
@@ -4,4 +4,4 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import torchtitan.experiments.simple_fsdp
+import torchtitan.experiments.simple_fsdp  # noqa: F401
--- torchtitan/tools/utils.py
+++ torchtitan/tools/utils.py
@@ -58,24 +58,6 @@ def get_num_params(model: nn.Module, exclude_embedding: bool = False) -> int:
     return num_params
 
 
-def get_num_flop_per_token(num_params: int, model_config, seq_len) -> int:
-    l, h, q, t = (
-        model_config.n_layers,
-        model_config.n_heads,
-        model_config.dim // model_config.n_heads,
-        seq_len,
-    )
-    # Reasoning behind the factor of 12 for the self-attention part of the formula:
-    # 1. each self-attention has 2 matmul in the forward and 4 in the backward (6)
-    # 2. the flash attention does 1 more matmul recomputation in the backward
-    #    but recomputation should not be counted in calculating MFU           (+0)
-    # 3. each matmul performs 1 multiplication and 1 addition                 (*2)
-    # 4. we follow the convention and do not account for sparsity in causal attention
-    flop_per_token = 6 * num_params + 12 * l * h * q * t
-
-    return flop_per_token
-
-
 # hardcoded BF16 type peak flops for NVIDIA A100, H100, H200 GPU and AMD MI250, MI300X and AMD MI325X
 def get_peak_flops(device_name: str) -> int:
     try:
