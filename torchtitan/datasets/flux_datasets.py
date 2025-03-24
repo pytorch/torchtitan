b43190a62aeb228a83c535d6c57f0c49b24b0236
Jiani Wang <jianiw@meta.com>
2025-03-24 13:22:09 -0700

    Adopt code form flux repo

--- torchtitan/datasets/flux_datasets.py
+++ torchtitan/datasets/flux_datasets.py
@@ -6,20 +6,18 @@
 
 import math
 from dataclasses import dataclass
-from typing import Any, Callable, List, Optional
+from typing import Any, Callable, Dict, List, Optional
 
 import torch
 
 from datasets import Dataset, load_dataset
 from datasets.distributed import split_dataset_by_node
-from einops import rearrange
+from einops import rearrange, repeat
 from torch.distributed.checkpoint.stateful import Stateful
-from torch.utils.data import IterableDataset
+from torchdata.stateful_dataloader import StatefulDataLoader
 
-from torchtitan.components.dataloader import ParallelAwareDataloader
-from torchtitan.components.tokenizer import Tokenizer
 from torchtitan.config_manager import JobConfig
-from torchtitan.datasets.utils import add_padding
+from torchtitan.datasets.tokenizer.HFEmbedder import HFEmbedder
 from torchtitan.tools.logging import logger
 
 
@@ -78,12 +76,14 @@ def _validate_dataset(
 
 
 def _get_noise_latent(
+    num_samples: int,
     height: int,
     width: int,
     dtype: torch.dtype,
     seed: int,
 ):
     return torch.randn(
+        num_samples,
         16,  # NOTE(jianiw): From the paper, d=16, h = H/8, w = W/8
         # allow for packing
         2 * math.ceil(height / 16),
@@ -93,16 +93,18 @@ def _get_noise_latent(
     )
 
 
-class HuggingFaceMultiModalDataset(IterableDataset, Stateful):
+class FLUXDataLoader(StatefulDataLoader, Stateful):
     def __init__(
         self,
         dataset_name: str,
         dataset_path: Optional[str],
-        tokenizer: List[Tokenizer],  # Tokenizer for text
+        embedder: List[HFEmbedder],  # Tokenizer for text
         seq_len: int = 2048,
         dp_rank: int = 0,
         dp_world_size: int = 1,
         infinite: bool = False,
+        batch_size: int = 1,
+        seed: int = 0,
     ) -> None:
         # Force lowercase for consistent comparison
         dataset_name = dataset_name.lower()
@@ -113,25 +115,27 @@ class HuggingFaceMultiModalDataset(IterableDataset, Stateful):
         ds = dataset_loader(path)
 
         # TODO(jianiw): Switch to use toml config to set the seed
-        self.seed = 0
+        self.seed = seed
         self.dataset_name = dataset_name
         self._data = split_dataset_by_node(ds, dp_rank, dp_world_size)
 
-        # TODO(jianiw): Find aa beter way to handle multiple tokenizers
-        assert len(tokenizer) == 2
+        assert len(embedder) == 2
 
-        self._t5_tokenizer = tokenizer[0]
-        self._clip_tokenizer = tokenizer[1]
-
-        self.seq_len = seq_len
-        self.infinite = infinite
+        self._t5_embedder = embedder[0]
+        self._clip_embedder = embedder[1]
         self._text_processor = text_processor
         self._image_processor = image_processor  # TODO(jianiw)
 
+        self.seq_len = seq_len
+        self.infinite = infinite
+        self.batch_size = batch_size
+
         # NOTE(jianiw): No need at this moment
         # # Variables for checkpointing
-        # self._sample_idx = 0
+        self._sample_idx = 0
         # self._all_tokens: list[int] = []
+        self._all_target_imgs: list[torch.Tensor] = []
+        self._all_prompts: list[str] = []
 
     def _get_data_iter(self):
         if isinstance(self._data, Dataset) and self._sample_idx == len(self._data):
@@ -142,68 +146,62 @@ class HuggingFaceMultiModalDataset(IterableDataset, Stateful):
             next(it)
         return it
 
+    def _prepare(self):
+        target_img = torch.stack(self._all_target_imgs)
+
+        sample_t5_embedding = self._t5_embedder(self._all_prompts)
+        txt_ids = torch.zeros(self.batch_size, sample_t5_embedding.shape[1], 3)
+        sample_clip_embedding = self._clip_tokenizer(self._all_prompts)
+
+        noise_latent = _get_noise_latent(
+            self.batch_size,
+            target_img.shape[0],
+            target_img.shape[1],
+            dtype=torch.bfloat16,
+            seed=self.seed,
+        )
+
+        _, _, h, w = noise_latent.shape  # (16, 256/8, 256/8) = (16, 32, 32)
+        # patchify the noise latent, p = 2
+        noise_latent = rearrange(
+            noise_latent, "b c (h ph) (w pw) -> b (h w) (c ph pw)", ph=2, pw=2
+        )
+
+        img_ids = torch.zeros(h // 2, w // 2, 3)
+        img_ids[..., 1] = img_ids[..., 1] + torch.arange(h // 2)[:, None]
+        img_ids[..., 2] = img_ids[..., 2] + torch.arange(w // 2)[None, :]
+        img_ids = repeat(img_ids, "h w c -> b (h w) c", b=self.batch_size)
+
+        return {
+            "img": noise_latent,
+            "img_ids": img_ids,
+            "txt": sample_t5_embedding,
+            "txt_ids": txt_ids,
+            "vec": sample_clip_embedding,
+            "target": target_img,
+        }
+
     def __iter__(self):
+        samples_cnt = 0
 
         while True:
             for sample in self._get_data_iter():
+                samples_cnt += 1
+
                 # Use the dataset-specific text processor
-                sample_text = self._text_processor(sample)
+                sample_promt = self._text_processor(sample)
                 sample_image = self._image_processor(sample)
 
-                # TODO(jianiw): Implement encoder, eg: https://github.com/black-forest-labs/flux/blob/716724eb276d94397be99710a0a54d352664e23b/src/flux/modules/conditioner.py
-                sample_t5_tokens = self._t5_tokenizer.encode(
-                    sample_text,
-                    padding="max_length",
-                    max_length=self.seq_len,
-                    return_overflowing_tokens=False,  # Do not return overflowing tokens
-                    return_length=False,
-                    return_tensors="pt",
-                )
+                self._all_target_imgs.extend(sample_image)
+                self._all_prompts.extend(sample_promt)
+                self._sample_idx += 1
 
-                # TODO: check the encoder output
-                txt_ids = torch.zeros(sample_t5_tokens.shape[1], 3)
-
-                # TODO(jianiw): We don't need to pad the tokens here, since the tokenizer  do it for us
-                # sample_t5_tokens = add_padding(
-                #     sample_t5_tokens, padding_item=0, target_len=self.seq_len
-                # )
-
-                sample_clip_tokens = self._clip_tokenizer.encode(
-                    sample_text,
-                    padding="max_length",
-                    max_length=self.seq_len,
-                    return_overflowing_tokens=False,  # Do not return overflowing tokens
-                    return_length=False,
-                    return_tensors="pt",
-                )
-
-                noise_latent = _get_noise_latent(
-                    sample_image.shape[0],  # TODO(jianiw): Check the noise latent shape
-                    sample_image.shape[1],
-                    dtype=torch.bfloat16,
-                    seed=self.seed,
-                )
-
-                _, h, w = noise_latent.shape  # (16, 256/8, 256/8) = (16, 32, 32)
-                # patchify the noise latent, p = 2
-                noise_latent = rearrange(
-                    noise_latent, "c (h ph) (w pw) -> (h w) (c ph pw)", ph=2, pw=2
-                )
-
-                # TODO(jianiw): Check what does this part of code do
-                img_id = torch.zeros(h // 2, w // 2, 3)
-                img_id[..., 1] = img_id[..., 1] + torch.arange(h // 2)[:, None]
-                img_id[..., 2] = img_id[..., 2] + torch.arange(w // 2)[None, :]
-                img_id = rearrange(img_id, "h w c -> (h w) c")
-
-                yield {
-                    "img": noise_latent,
-                    "img_id": img_id,
-                    "txt": sample_t5_tokens,
-                    "txt_id": txt_id,
-                    "vec": sample_clip_tokens,
-                    "target": sample_image,
-                }
+                if samples_cnt == self.batch_size:
+                    batched_data = self._prepare()
+                    samples_cnt = 0
+                    self._all_prompts = []
+                    self._all_target_imgs = []
+                    yield batched_data
 
             if not self.infinite:
                 logger.warning(f"Dataset {self.dataset_name} has run out of data")
@@ -221,32 +219,28 @@ class HuggingFaceMultiModalDataset(IterableDataset, Stateful):
         pass
 
 
-def build_hf_dataloader(
+def build_flux_dataloader(
     dp_world_size: int,
     dp_rank: int,
-    tokenizer: List[Tokenizer],
+    embedder: List[HFEmbedder],
     job_config: JobConfig,
     infinite: bool = True,
-) -> ParallelAwareDataloader:
+) -> FLUXDataLoader:
     """Build a data loader for HuggingFace datasets."""
     dataset_name = job_config.training.dataset
     dataset_path = job_config.training.dataset_path
     batch_size = job_config.training.batch_size
     seq_len = job_config.training.seq_len
+    seed = job_config.training.seed
 
-    hf_ds = HuggingFaceDataset(
+    return FLUXDataLoader(
         dataset_name=dataset_name,
         dataset_path=dataset_path,
-        tokenizer=tokenizer,
+        embedder=embedder,
         seq_len=seq_len,
         dp_rank=dp_rank,
         dp_world_size=dp_world_size,
         infinite=infinite,
-    )
-
-    return ParallelAwareDataloader(
-        dataset=hf_ds,
-        dp_rank=dp_rank,
-        dp_world_size=dp_world_size,
         batch_size=batch_size,
+        seed=seed,
     )
