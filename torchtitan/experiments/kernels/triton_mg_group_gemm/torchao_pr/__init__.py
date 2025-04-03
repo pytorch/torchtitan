7c7923fbe94ff9617706cfda7ee392e6033d5c8c
Less Wright <lessw@etrillium.com>
2025-04-03 14:42:14 -0700

    [DeepSeek] remove extra development code (#1051)
    Remove extra workspace and development code.  
    This was from a bit rushed landing, so just keeping the core files now.

--- torchtitan/experiments/kernels/triton_mg_group_gemm/torchao_pr/__init__.py
+++ torchtitan/experiments/kernels/triton_mg_group_gemm/torchao_pr/__init__.py
@@ -3,11 +3,3 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
-# pyre-unsafe
-import logging
-import unittest
-from typing import Tuple
-
-import torch
-import torch.nn as nn
