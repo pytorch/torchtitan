82ef16a9c3eb133e2cece7c98462a04127d3780f
tianyu-l <150487191+tianyu-l@users.noreply.github.com>
2025-04-04 14:27:25 -0700

    add async TP test to CI (#1029)

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -97,6 +97,17 @@ def build_test_list():
         OverrideDefinitions(
             [
                 [
+                    "--training.compile",
+                    "--parallelism.tensor_parallel_degree 2",
+                    "--parallelism.enable_async_tensor_parallel",
+                ],
+            ],
+            "2D async TP compile",
+            "2d_asynctp_compile",
+        ),
+        OverrideDefinitions(
+            [
+                [
                     "--checkpoint.enable_checkpoint",
                 ],
                 [
