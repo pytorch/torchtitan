52a1a335ac423d588c760aa3f1ac6a7398950531
Chien-Chin Huang <chienchin@fb.com>
2025-04-16 14:40:49 -0700

    docstring

--- torchtitan/distributed/utils.py
+++ torchtitan/distributed/utils.py
@@ -25,25 +25,39 @@ def _dist_reduce(
     x: torch.Tensor,
     reduceOp: str,
     mesh: DeviceMesh,
-    pg: dist.ProcessGroup | None = None,
+    extra_pg: dist.ProcessGroup | None = None,
 ) -> float:
+    """Perform distributed reduction on a tensor.
+
+    Args:
+        x (torch.Tensor): Input tensor.
+        reduceOp (str): Reduce operation to perform.
+        mesh (DeviceMesh): Device mesh to use for reduction.
+        extra_pg (dist.ProcessGroup, optional): Extra process group to use for reduction.
+            Defaults to None. If provided, this all_reduce will be called for the extra
+            process group, and then the result will be all_reduced for the mesh.
+    """
     if isinstance(x, DTensor):
         # functional collectives do not support DTensor inputs
         x = x.full_tensor()
 
-    if pg is not None:
-        x = funcol.all_reduce(x, reduceOp=reduceOp, group=pg)
+    if extra_pg is not None:
+        x = funcol.all_reduce(x, reduceOp=reduceOp, group=extra_pg)
 
     assert x.numel() == 1  # required by `.item()`
     return funcol.all_reduce(x, reduceOp=reduceOp, group=mesh).item()
 
 
-def dist_max(x: torch.Tensor, mesh: DeviceMesh, pg: dist.ProcessGroup | None) -> float:
-    return _dist_reduce(x, reduceOp=c10d.ReduceOp.MAX.name, mesh=mesh, pg=pg)
+def dist_max(
+    x: torch.Tensor, mesh: DeviceMesh, extra_pg: dist.ProcessGroup | None
+) -> float:
+    return _dist_reduce(x, reduceOp=c10d.ReduceOp.MAX.name, mesh=mesh, extra_pg=pg)
 
 
-def dist_mean(x: torch.Tensor, mesh: DeviceMesh, pg: dist.ProcessGroup | None) -> float:
-    return _dist_reduce(x, reduceOp=c10d.ReduceOp.AVG.name, mesh=mesh, pg=pg)
+def dist_mean(
+    x: torch.Tensor, mesh: DeviceMesh, extra_pg: dist.ProcessGroup | None
+) -> float:
+    return _dist_reduce(x, reduceOp=c10d.ReduceOp.AVG.name, mesh=mesh, extra_pg=pg)
 
 
 def set_determinism(
