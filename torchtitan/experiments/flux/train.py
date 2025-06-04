12898e471a2c57eb71a7ba3e96671a179c683b66
janEbert <janpublicebert@posteo.net>
2025-06-04 13:57:10 +0200

    Refactor FLUX train step
    ... toward `forward_backward_step` design.

--- torchtitan/experiments/flux/train.py
+++ torchtitan/experiments/flux/train.py
@@ -81,10 +81,9 @@ class FluxTrainer(Trainer):
             job_config=job_config,
         )
 
-    def train_step(
-        self, data_iterator: Iterable[tuple[dict[str, torch.Tensor], torch.Tensor]]
+    def forward_backward_step(
+        self, input_dict: dict[str, torch.Tensor], labels: torch.Tensor
     ):
-        input_dict, labels = self.next_batch(data_iterator)
         # generate t5 and clip embeddings
         input_dict["image"] = labels
         input_dict = preprocess_data(
@@ -97,17 +96,10 @@ class FluxTrainer(Trainer):
         )
         labels = input_dict["img_encodings"]
 
-        self.optimizers.zero_grad()
-
         # Keep these variables local to shorten the code as these are
         # the major variables that are used in the training loop.
-        model_parts = self.model_parts
-        assert len(self.model_parts) == 1
         # explicitely convert flux model to be Bfloat16 no matter FSDP is applied or not
         model = self.model_parts[0]
-
-        world_mesh = self.world_mesh
-        parallel_dims = self.parallel_dims
 
         # image in latent space transformed by self.auto_encoder
         clip_encodings = input_dict["clip_encodings"]
@@ -151,6 +143,27 @@ class FluxTrainer(Trainer):
         # need to free to before bwd to avoid peaking memory
         del (pred, noise, target)
         loss.backward()
+
+        return loss
+
+    def train_step(
+        self, data_iterator: Iterable[tuple[dict[str, torch.Tensor], torch.Tensor]]
+    ):
+        input_dict, labels = self.next_batch(data_iterator)
+
+        self.optimizers.zero_grad()
+
+        # Keep these variables local to shorten the code as these are
+        # the major variables that are used in the training loop.
+        model_parts = self.model_parts
+        assert len(self.model_parts) == 1
+        # explicitely convert flux model to be Bfloat16 no matter FSDP is applied or not
+        model = self.model_parts[0]
+
+        world_mesh = self.world_mesh
+        parallel_dims = self.parallel_dims
+
+        loss = self.forward_backward_step(input_dict, labels)
 
         dist_utils.clip_grad_norm_(
             [p for m in model_parts for p in m.parameters()],
