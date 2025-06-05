2e39698b4c5d73c8aaf42be05579dc74bd2bf6bc
Chien-Chin Huang <chienchin@fb.com>
2025-06-05 00:11:47 -0700

    Fixes the option names

--- torchtitan/experiments/flux/tests/integration_tests.py
+++ torchtitan/experiments/flux/tests/integration_tests.py
@@ -58,11 +58,11 @@ def build_test_list():
             [
                 [
                     "--checkpoint.enable_checkpoint",
-                    "--checkpoint.model_weights_only",
+                    "--checkpoint.save_model_weights_only",
                 ],
             ],
             "Checkpoint Integration Test - Save Model Weights Only fp32",
-            "model_weights_only_fp32",
+            "save_model_weights_only_fp32",
         ),
         # Parallelism tests.
         OverrideDefinitions(
