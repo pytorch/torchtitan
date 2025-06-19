c8e302fa357ccc66f9e598bc96430d3cf5726d8d
Jiani Wang <jianiw@meta.com>
2025-06-19 08:42:23 -0700

    lint

--- torchtitan/models/deepseek_v3/infra/parallelize.py
+++ torchtitan/models/deepseek_v3/infra/parallelize.py
@@ -4,18 +4,13 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import torch
+
 import torch.nn as nn
-from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
-    checkpoint_wrapper as ptd_checkpoint_wrapper,
-)
 
 from torch.distributed.device_mesh import DeviceMesh
-from torch.distributed.fsdp import CPUOffloadPolicy, fully_shard, MixedPrecisionPolicy
 
-from torchtitan.config_manager import JobConfig, TORCH_DTYPE_MAP
+from torchtitan.config_manager import JobConfig
 from torchtitan.distributed import ParallelDims
-from torchtitan.tools.logging import logger
 
 
 def parallelize_deepseekv3(
