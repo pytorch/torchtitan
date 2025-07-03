56bf9303c3616fa4b1d2dd0d85abd3e2b7d62ec0
Akash Verma <Akash.Verma3@amd.com>
2025-07-03 00:14:42 -0500

    Resolved conflict in .github.workflows.integration_test_8gpu_models during rebase.

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -12,27 +12,21 @@ shift
 
 echo "Building ${IMAGE_NAME} Docker image"
 
-# set operating system
 OS=ubuntu
-
-# set Dockerfile
-DOCKERFILE="${OS}/Dockerfile"
-if [[ "$IMAGE_NAME" == *rocm* ]]; then
-  DOCKERFILE="${OS}-rocm/Dockerfile"
-fi
+CLANG_VERSION=""
+PYTHON_VERSION=3.11
+MINICONDA_VERSION=24.3.0-0
 
 case "${IMAGE_NAME}" in
   torchtitan-ubuntu-20.04-clang12)
     OS_VERSION=20.04
     CLANG_VERSION=12
-    PYTHON_VERSION=3.11
-    MINICONDA_VERSION=24.3.0-0
+    BASE_IMAGE=nvidia/cuda:12.4.1-cudnn-runtime-ubuntu${OS_VERSION}
     ;;
-  torchtitan-rocm-pytorch-nightly-ubuntu-22.04-clang19-py3)
+  torchtitan-rocm-ubuntu-22.04-clang12)
     OS_VERSION=22.04
-    CLANG_VERSION=19
-    PYTHON_VERSION=3.10
-    MINICONDA_VERSION=25.3.1-0
+    CLANG_VERSION=12
+    BASE_IMAGE=rocm/dev-ubuntu-${OS_VERSION}:latest
     ;;
   *)
     echo "Invalid image name ${IMAGE_NAME}"
@@ -42,11 +36,13 @@ esac
 docker build \
   --no-cache \
   --progress=plain \
+  --build-arg "BASE_IMAGE=${BASE_IMAGE}" \
   --build-arg "OS_VERSION=${OS_VERSION}" \
   --build-arg "CLANG_VERSION=${CLANG_VERSION}" \
   --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" \
   --build-arg "MINICONDA_VERSION=${MINICONDA_VERSION}" \
-  -f $(dirname ${DOCKERFILE})/Dockerfile \
+  --shm-size=1g \
+  -f "${OS}"/Dockerfile \
   "$@" \
   .
 
