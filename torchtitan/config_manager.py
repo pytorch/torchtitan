9fa998a90ab3a9a007785d0cb6a0da22856a5363
soulitzer <soulitzer@gmail.com>
2025-07-08 12:07:13 -0700

    Update
    [ghstack-poisoned]

--- torchtitan/config_manager.py
+++ torchtitan/config_manager.py
@@ -489,11 +489,8 @@ class ActivationCheckpoint:
     selective_op_ac_mm_flops_threshold: int = 0
     """
     When selective_ac_option is 'op', this threshold is used to determine whether to
-    apply save a given mm.
-
-    For example:
-    - 0 means no threshold; every other mm is saved
-    - 1e5 means every other mm is saved, excluding mm with flops > 1e5.
+    save a given mm, e.g. 1e5 means every other mm is saved, excluding mm with
+    flops < 1e5.
     """
 
 
