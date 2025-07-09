fef4d7be962f89d359c748b99523a084935ce3f9
CarlosGomes98 <carlosmiguel.gomes@live.com.pt>
2025-07-09 11:42:49 +0200

    make eval target loss explicit in config

--- torchtitan/experiments/flux/job_config.py
+++ torchtitan/experiments/flux/job_config.py
@@ -91,7 +91,7 @@ class Eval:
     """Path to the prompts csv file."""
     num_prompts: int | None = None
     """Number of prompts to use for validation."""
-    target_eval_loss: float = 0.591
+    target_eval_loss: float = 0.586
     """Target loss for validation. If the validation loss is less than this value, the training will be considered successful."""
 
 
