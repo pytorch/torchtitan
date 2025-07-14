cc8e17b45271cd1a60f2e96130bc238984a6b487
joecummings <jrcummings27@gmail.com>
2025-07-14 08:53:05 -0700

    Ensure there's a __version__ variable available through titan

--- .github/scripts/update_version.sh
+++ .github/scripts/update_version.sh
@@ -1,6 +1,10 @@
-python_file="assets/version.txt"
+version_file="assets/version.txt"
+init_file="torchtitan/__init__.py"
 if [[ -n "$BUILD_VERSION" ]]; then
-    echo "$BUILD_VERSION" > "$python_file"
+    # Update the version in version.txt
+    echo "$BUILD_VERSION" > "$version_file"
+    # Create a variable named __version__ at the end of __init__.py
+    echo "__version__ = \"$BUILD_VERSION\"" >> "$init_file"
 else
     echo "Error: BUILD_VERSION environment variable is not set or empty."
     exit 1
