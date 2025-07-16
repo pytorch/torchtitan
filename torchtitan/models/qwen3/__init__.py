5235f40113f14cfdc76858a6b0f9fcb55af8f6d5
Hossein Kavianihamedani <hosseinkh@fb.com>
2025-07-16 11:27:42 -0700

    TorchTitanQwen

--- torchtitan/models/qwen3/__init__.py
+++ torchtitan/models/qwen3/__init__.py
@@ -69,25 +69,3 @@ register_train_spec(
         build_validator_fn=build_validator,
     )
 )
-
-
-"""
-
-CHOOSE_MODEL = "0.6B"
-
-if CHOOSE_MODEL == "0.6B":
-    QWEN3_CONFIG = {
-        "vocab_size": 151_936,           # Vocabulary size
-        "context_length": 40_960,        # Context length that was used to train the model
-        "emb_dim": 1024,                 # Embedding dimension
-        "n_heads": 16,                   # Number of attention heads
-        "n_layers": 28,                  # Number of layers
-        "hidden_dim": 3072,              # Size of the intermediate dimension in FeedForward
-        "head_dim": 128,                 # Size of the heads in GQA
-        "qk_norm": True,                 # Whether to normalize queries and values in GQA
-        "n_kv_groups": 8,                # Key-Value groups for grouped-query attention
-        "rope_base": 1_000_000.0,        # The base in RoPE's "theta"
-        "dtype": torch.bfloat16,         # Lower-precision dtype to reduce memory usage
-    }
-
-"""
