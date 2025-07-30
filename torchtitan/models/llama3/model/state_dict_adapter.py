46b5cd71f1219a1a2de102b370988ac1a6d34377
Jiani Wang <jianiw@meta.com>
2025-07-29 17:48:08 -0700

    change state_dict_adapter

--- torchtitan/models/llama3/model/state_dict_adapter.py
+++ torchtitan/models/llama3/model/state_dict_adapter.py
@@ -10,7 +10,19 @@ from typing import Any
 from torchtitan.protocols.state_dict_adapter import StateDictAdapter
 
 from .args import TransformerModelArgs
+from torchtitan.tools.logging import logger
+import torch
+import torch.nn.functional as F
 
+
+def loss_fn(logits1, logits2):
+    # Convert logits to probabilities
+    probs1 = F.log_softmax(logits1, dim=-1)
+    probs2 = F.softmax(logits2, dim=-1)
+
+    # Calculate KL Divergence
+    kl_loss = F.kl_div(probs1, probs2, "mean")
+    return kl_loss
 
 class Llama3StateDictAdapter(StateDictAdapter):
     def __init__(self, model_args: TransformerModelArgs):
@@ -76,10 +88,28 @@ class Llama3StateDictAdapter(StateDictAdapter):
                 # We need to permute the weights in wq and wk layer in order to account for the difference between
                 # the native Llama and huggingface RoPE implementation.
                 if abstract_key == "layers.{}.attention.wq.weight":
+                    input_full_tensor = value.clone().full_tensor()
+                    logger.info(f"[To hf] Before permute {key}, the dtensor shape is {value.shape}, placement is {value.placements}, device_mesh is {value.device_mesh}")
                     value = self._permute(value, n_heads)
+                    output_full_tensor = value.clone().full_tensor()
+                    logger.info(f"[To hf] After permute {key}, the dtensor shape is {value.shape}, placement is {value.placements}, device_mesh is {value.device_mesh}")
+                    # logger.info(f"[To hf] KL divergence between input and output tensors for {key}: {loss_fn(self._permute(input_full_tensor, n_heads), output_full_tensor).item()}")
+                    are_tensors_equal = torch.allclose(
+                        self._permute(input_full_tensor, n_heads), 
+                        output_full_tensor,
+                        rtol=1e-5, 
+                        atol=1e-5
+                    )
+                    logger.info(f"[To hf] Are input and output tensors equivalent for {key}: {are_tensors_equal}")
+                    
                 if abstract_key == "layers.{}.attention.wk.weight":
+                    input_full_tensor = value.clone().full_tensor()
                     key_value_dim = head_dim * n_kv_heads
+                    logger.info(f"[To hf] Before permute {key}, the dtensor shape is {value.shape}, placement is {value.placements}, device_mesh is {value.device_mesh}")
                     value = self._permute(value, n_kv_heads, key_value_dim, dim)
+                    output_full_tensor = value.clone().full_tensor()
+                    logger.info(f"[To hf] After permute {key}, the dtensor shape is {value.shape}, placement is {value.placements}, device_mesh is {value.device_mesh}")
+                    logger.info(f"[To hf] KL divergence between input and output tensors for {key}: {loss_fn(self._permute(input_full_tensor, n_heads), output_full_tensor).item()}")
 
                 if new_key is None:
                     continue
@@ -111,10 +141,16 @@ class Llama3StateDictAdapter(StateDictAdapter):
                 # We need to permute the weights in wq and wk layer in order to account for the difference between
                 # the native Llama and huggingface RoPE implementation.
                 if abstract_key == "model.layers.{}.self_attn.q_proj.weight":
+                    logger.info(f"[From hf] Before _reverse_permute {abstract_key}, the dtensor shape is {value.shape}, placement is {value.placements}, device_mesh is {value.device_mesh}")
                     value = self._reverse_permute(value, n_heads)
+                    logger.info(f"[From hf] After _reverse_permute {abstract_key}, the dtensor shape is {value.shape}, placement is {value.placements}, device_mesh is {value.device_mesh}")
+
                 if abstract_key == "model.layers.{}.self_attn.k_proj.weight":
                     key_value_dim = head_dim * n_kv_heads
+                    logger.info(f"[From hf] Before _reverse_permute {abstract_key}, the dtensor shape is {value.shape}, placement is {value.placements}, device_mesh is {value.device_mesh}")
                     value = self._reverse_permute(value, n_kv_heads, key_value_dim, dim)
+                    logger.info(f"[From hf] After _reverse_permute {abstract_key}, the dtensor shape is {value.shape}, placement is {value.placements}, device_mesh is {value.device_mesh}")
+
 
                 if new_key is None:
                     continue
