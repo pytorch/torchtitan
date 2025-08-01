7eba53d7dfb13078cfdab845cbfd793c53921939
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-08-01 15:22:19 -0700

    separate out diloco configs

---
+++ torchtitan/components/ft/config/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Import to register quantization modules.
+from torchtitan.components.ft.config.job_config import FaultTolerance, JobConfig
+
+
+__all__ = [
+    "FaultTolerance",
+    "JobConfig",
+]
