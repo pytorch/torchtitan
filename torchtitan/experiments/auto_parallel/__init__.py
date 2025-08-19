91c56397e8f7c98ec2c685bdf551637f8780b56c
Will Constable <whc@meta.com>
2025-08-19 13:46:38 -0700

    Hook up deepseekv3_auto_parallel
    This command should now run
    
    `CONFIG_FILE="./torchtitan/models/deepseek_v3/train_configs/debug_model.toml"
    ./run_train.sh --model.name deepseekv3_auto_parallel`
    
    However it doesn't actually do anything with autoparallel yet. Next step
    is to attach local_map to the model so that autoparallel can run.

--- torchtitan/experiments/auto_parallel/__init__.py
+++ torchtitan/experiments/auto_parallel/__init__.py
@@ -13,7 +13,11 @@ from torchtitan.components.tokenizer import build_hf_tokenizer
 from torchtitan.datasets.hf_datasets import build_hf_dataloader
 from torchtitan.models.llama3 import llama3_configs, pipeline_llama, Transformer
 from torchtitan.protocols.train_spec import register_train_spec, TrainSpec
+from torchtitan.experiments.llama4.optimizer import build_llama4_optimizers
+from torchtitan.models.deepseek_v3 import deepseekv3_configs, DeepSeekV3Model
 from .parallelize_llama import parallelize_llama
+from .parallelize_deepseekv3 import parallelize_deepseekv3
+
 
 register_train_spec(
     TrainSpec(
@@ -23,6 +27,20 @@ register_train_spec(
         parallelize_fn=parallelize_llama,
         pipelining_fn=pipeline_llama,
         build_optimizers_fn=build_optimizers,
+        build_lr_schedulers_fn=build_lr_schedulers,
+        build_dataloader_fn=build_hf_dataloader,
+        build_tokenizer_fn=build_hf_tokenizer,
+        build_loss_fn=build_cross_entropy_loss,
+    )
+)
+register_train_spec(
+    TrainSpec(
+        name="deepseekv3_auto_parallel",
+        cls=DeepSeekV3Model,
+        config=deepseekv3_configs,
+        parallelize_fn=parallelize_deepseekv3,
+        pipelining_fn=None,
+        build_optimizers_fn=build_llama4_optimizers,  # use optimizer hooks to update expert weights
         build_lr_schedulers_fn=build_lr_schedulers,
         build_dataloader_fn=build_hf_dataloader,
         build_tokenizer_fn=build_hf_tokenizer,
