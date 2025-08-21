cad098a8be2f131cefa535be18c57faa2d5df00b
Phúc Lê Khắc <phuc.lekhac@tii.ae>
2025-08-21 15:38:08 +0400

    [vlm] model code w/ siglip2 encoder
    tmp

---
+++ torchtitan/experiments/vlm/model/args.py
@@ -0,0 +1,31 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from dataclasses import dataclass, field
+
+from torchtitan.models.llama3 import TransformerModelArgs as Llama3Args
+
+
+@dataclass
+class Siglip2ModelArgs:
+    dim: int = 768
+    ffn_dim: int = 3072
+    n_layers: int = 12
+    n_heads: int = 12
+
+    n_pos_embs: int = 16  # Number of positional embeddings per h&w
+    n_channels: int = 3  # RGB channels
+    patch_size: int = 16
+
+    layer_norm_eps: float = 1e-6
+    use_flex_attn: bool = True
+    attn_mask_type: str = "causal"
+
+
+@dataclass
+class Llama3Siglip2ModelArgs(Llama3Args):
+    encoder: Siglip2ModelArgs = field(default_factory=Siglip2ModelArgs)
+    img_token_id: int = 1998
