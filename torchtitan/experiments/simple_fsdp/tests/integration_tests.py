08b8b244f3a63dfb606444dbe18bddaedd04ce1e
tianyu-l <150487191+tianyu-l@users.noreply.github.com>
2025-08-20 19:00:21 -0700

    [refactor] support compile model and loss separately (#1608)
    Creating a new field in `JobConfig`, with the default being
    ```
    [compile]
    enable=false
    components = ["model", "loss"]
    ```
    
    This way we get to compile loss separately to get memory reduction, even
    when the model is not ready to be compiled.
    
    This PR also applies loss compilation to DeepSeek 16B and 671B.

--- torchtitan/experiments/simple_fsdp/tests/integration_tests.py
+++ torchtitan/experiments/simple_fsdp/tests/integration_tests.py
@@ -220,7 +220,7 @@ def run_test(test_flavor: OverrideDefinitions, full_path: str, output_dir: str):
     for idx, override_arg in enumerate(test_flavor.override_args):
         cmd = (
             f"CONFIG_FILE={full_path} NGPU={test_flavor.ngpu} LOG_RANK={all_ranks} ./run_train.sh "
-            f"--model.name llama3_simple_fsdp --training.compile "
+            f"--model.name llama3_simple_fsdp --compile.enable "
         )
         # dump compile trace for debugging purpose
         cmd = f'TORCH_TRACE="{output_dir}/{test_name}/compile_trace" ' + cmd
