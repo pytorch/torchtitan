2b46afb27ed86b4c179e9576025b8ba8c1913668
Jiani Wang <jianiw@meta.com>
2025-09-01 15:08:38 -0700

    fix nparams

--- torchtitan/experiments/qwen3/model/args.py
+++ torchtitan/experiments/qwen3/model/args.py
@@ -65,8 +65,7 @@ class Qwen3ModelArgs(BaseModelArgs):
         num_flops_per_token = 6 * (nparams - nparams_embedding) + 12 * l * h * q * t
 
         if self.enable_weight_tying:
-            nparams = nparams - sum(
-                p.numel() for p in model.tok_embeddings.parameters()
-            )
+            # exclude model.token_embedding parameters from nparams
+            nparams = nparams - nparams_embedding
 
         return nparams, num_flops_per_token
