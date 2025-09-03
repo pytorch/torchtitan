4f1485d4c9506e4d48aff2d570eed160d880bb88
Chien-Chin Huang <chienchin@fb.com>
2025-09-03 16:11:19 -0700

    misc

--- torchtitan/distributed/__init__.py
+++ torchtitan/distributed/__init__.py
@@ -5,6 +5,8 @@
 # LICENSE file in the root directory of this source tree.
 
 
+from functools import partial
+
 import torch.nn as nn
 from torch.distributed.tensor import DeviceMesh, distribute_module, DTensor, Replicate
 from torch.distributed.tensor.parallel import ParallelStyle
