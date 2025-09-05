386f8abec41213fd59e893f56f2ae8bc5fbe20c8
Chien-Chin Huang <chienchin@fb.com>
2025-09-04 18:06:49 -0700

    [Don't review yet] SAC + Flex refactoring
    TBD

--- torchtitan/models/deepseek_v3/__init__.py
+++ torchtitan/models/deepseek_v3/__init__.py
@@ -74,8 +74,6 @@ deepseekv3_configs = {
         qk_rope_head_dim=64,
         v_head_dim=128,
         mscale=0.70,
-        use_flex_attn=True,
-        attn_mask_type="block_causal",
     ),
     "16B": DeepSeekV3ModelArgs(
         vocab_size=102400,
