4b498a94fa9a02679005147cc6ae23460e11af45
3outeille <ferdinand.mom@epita.fr>
2025-09-16 12:05:49 +0000

    no need to fill passed args

--- torchtitan/experiments/transformers_backend/__init__.py
+++ torchtitan/experiments/transformers_backend/__init__.py
@@ -18,36 +18,25 @@ from torchtitan.protocols.train_spec import register_train_spec, TrainSpec
 from .infra.parallelize_hf_transformers import parallelize_hf_transformers
 from .model.hf_transformers_args import HFTransformerModelArgs, HFTransformerModel
 
-from transformers.models.llama.modeling_llama import LlamaForCausalLM
-
-
 __all__ = [
     "HFTransformerModelArgs",
     "HFTransformerModel",
     "hf_transformers_configs",
 ]
 
-
-def hf_transformer_model_args_builder(**kwargs):
-    # Capture the kwargs in the passed_args field
-    args = HFTransformerModelArgs(**kwargs)
-    args.passed_args = kwargs
-    return args
-
-
 flavors = {
-    "debugmodel": hf_transformer_model_args_builder(
+    "debugmodel": HFTransformerModelArgs(
         # n_layers=2,
         # vocab_size=2000,
         max_seq_len=2048,
         #TODO(3outeille): n_kv_heads=n_heads may be handle somewhere else
         dim=256, n_layers=6, n_heads=16, vocab_size=2000, rope_theta=500000, n_kv_heads=16
     ),
-    "medium": hf_transformer_model_args_builder(
+    "medium": HFTransformerModelArgs(
         dim=1024,
         n_layers=12,
     ),
-    "full": hf_transformer_model_args_builder(),
+    "full": HFTransformerModelArgs(),
 }
 
 hf_train_spec = TrainSpec(
