47bb79ca95883548f1b1b5f5b667afcabf6f970e
Tianyu Liu <lty@fb.com>
2025-09-22 21:34:07 -0700

    [refactor] dynamically import TrainSpec

--- torchtitan/__init__.py
+++ torchtitan/__init__.py
@@ -6,8 +6,3 @@
 
 # Import to register quantization modules.
 import torchtitan.components.quantization  # noqa: F401
-
-# Import the built-in models here so that the corresponding register_model_spec()
-# will be called.
-import torchtitan.experiments  # noqa: F401
-import torchtitan.models  # noqa: F401
