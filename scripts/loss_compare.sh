60cb2e47f914cd6574d30eafa92cfc5c7080191b
Chien-Chin Huang <chienchin@fb.com>
2025-10-05 11:03:10 -0700

    license

--- scripts/loss_compare.sh
+++ scripts/loss_compare.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 # This script compares training losses between different git commits and/or different training configurations.
 # --training.deterministic is always enabled and seed checkpoint is also enabled by default for reproducible
 # comparisons. You can disable seed checkpoint with --no-seed-checkpoint if you don't need it to speed up comparisons.
