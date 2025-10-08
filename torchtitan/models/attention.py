a04b7a6c649aafa1a35075971b0a02eae45a1ab7
Chien-Chin Huang <chienchin@fb.com>
2025-10-08 14:11:57 -0700

    Update
    [ghstack-poisoned]

--- torchtitan/models/attention.py
+++ torchtitan/models/attention.py
@@ -90,7 +90,6 @@ class ScaledDotProductAttentionWrapper(torch.nn.Module):
                 SDPBackend.CUDNN_ATTENTION,
                 SDPBackend.FLASH_ATTENTION,
                 SDPBackend.EFFICIENT_ATTENTION,
-                SDPBackend.MATH,
             ]
 
     def forward(
