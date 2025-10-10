6a3a9da9564d82a1120c7639ef6236bb4cffa049
Chien-Chin Huang <fegins@gmail.com>
2025-10-09 23:03:57 -0700

    Refactor attention and make attention mask an argument to the model (#1776)
    Stack from [ghstack](https://github.com/ezyang/ghstack/tree/0.12.0)
    (oldest at bottom):
    * #1797
    * __->__ #1776
    
    **Status**
    1. Change all models, including the experimental ones.
    2. E2E loss verification.
    3. We should add an unittest for attention. But since we don't have GPU
    unittest, this can be done in a separate PR.
    
    **Summary**
    This PR aims to refactor how TorchTitan build the attention masks and
    pass to model. Before this PR, init_attention_masks() is called in
    Trainer but the masks are stored as a class variable of
    FlexAttentionWrapper(). We chose this shortcut to support the case where
    a single model requires multiple masks.
    
    The previous design has several issues, one particular one is
    https://github.com/pytorch/torchtitan/issues/1723.
    
    https://github.com/pytorch/pytorch/pull/164111/ proves that we can let
    PP split BlockMask, this PR performs the refactor to pass masks as an
    argument of model.forward().
    
    The new design:
    1. Model needs to provide `get_attention_masks()` that accepts
    `create_mask_fn`, `batch`, and `eos_id`. If the attention op is SDPA,
    then this API should return None as SDPA currently doesn't support
    varlen. But once it does, we may have to return some tuple of int that
    represents the mask.
    
    Justification: attention logic is technically a part of the model, but
    requires some information from trainer/dataloader. So it's model
    author's responsibility to provide some API that let trainer calls to
    get the masks.
    
    2. `get_attention_masks()` will be called from the trainer and the
    resulting masks are passed to the model.forward().
    
    Justification: this will allow us to fix
    https://github.com/pytorch/torchtitan/issues/1723 with
    https://github.com/pytorch/pytorch/pull/164111/ and this PR.
    
    3. Now SDPA and FlexAttention are wrapped in two different classes.
    ~~Note: we still have two very very thin op wrappers that are used for
    CP. I keep these two for the CP education purpose. But this certainly
    can be confusion for Titan's users. I'm opnn to merge them to
    AttentionOp.~~
    
    See the discussion in https://github.com/pytorch/torchtitan/issues/1723.
    
    
    **Verification**
    *llama3*
    ```
    ./loss_compare.sh main 9dc16675b272ffdc3ed616e3244bcf7dc2d257f2 --steps=100 --no-seed-checkpoint --config="./torchtitan/models/llama3/train_configs/debug_model.toml"
    ```
    *llama3 flex*
    ```
    ./loss_compare.sh main 9dc16675b272ffdc3ed616e3244bcf7dc2d257f2 --steps=100 --no-seed-checkpoint --config="./torchtitan/models/llama3/train_configs/debug_model.toml" --baseline-train-options="--model.flavor=debugmodel_flex_attn"
    ```
    *llama4*
    ```
    ./loss_compare.sh main 9dc16675b272ffdc3ed616e3244bcf7dc2d257f2 --steps=100 --no-seed-checkpoint 
    ```
    *llama4 irope*
    ```
    ./loss_compare.sh main 9dc16675b272ffdc3ed616e3244bcf7dc2d257f2 --steps=100 --no-seed-checkpoint 
    ```
    *deepseek*
    ```
    ./loss_compare.sh main 9dc16675b272ffdc3ed616e3244bcf7dc2d257f2 --steps=100 --no-seed-checkpoint --config="./torchtitan/models/deepseek_v3/train_configs/debug_model.toml"
    ```
    *deepseek flex*
    ```
    ./loss_compare.sh main 9dc16675b272ffdc3ed616e3244bcf7dc2d257f2 --steps=100 --no-seed-checkpoint --config="./torchtitan/models/deepseek_v3/train_configs/debug_model.toml" --baseline-train-options="--model.flavor=debugmodel_flex_attn"
    ```

--- torchtitan/models/llama3/model/model.py
+++ torchtitan/models/llama3/model/model.py
@@ -11,8 +11,17 @@ import math
 import torch
 import torch.nn.functional as F
 from torch import nn
+from torch.nn.attention.flex_attention import and_masks, BlockMask
 
-from torchtitan.models.attention import build_attention
+from torchtitan.components.tokenizer import BaseTokenizer
+from torchtitan.models.attention import (
+    create_attention_mask,
+    FlexAttentionWrapper,
+    get_causal_mask_mod,
+    get_document_mask_mod,
+    ScaledDotProductAttentionWrapper,
+)
+from torchtitan.protocols.model import AttentionMasksType
 from torchtitan.protocols.train_spec import ModelProtocol
 
 from .args import RoPEScalingArgs, TransformerModelArgs
@@ -181,7 +190,12 @@ class Attention(nn.Module):
         self.wo = nn.Linear(
             model_args.n_heads * self.head_dim, model_args.dim, bias=False
         )
-        self.sdpa = build_attention(model_args.use_flex_attn, model_args.attn_mask_type)
+
+        self.use_flex_attn = model_args.use_flex_attn
+        if self.use_flex_attn:
+            self.inner_attention = FlexAttentionWrapper()
+        else:
+            self.inner_attention = ScaledDotProductAttentionWrapper()
 
     def init_weights(self, init_std: float):
         for linear in (self.wq, self.wk, self.wv):
@@ -192,6 +206,7 @@ class Attention(nn.Module):
         self,
         x: torch.Tensor,
         freqs_cis: torch.Tensor,
+        attention_masks: AttentionMasksType | None,
     ):
         """
         Forward pass of the attention module.
@@ -225,7 +240,16 @@ class Attention(nn.Module):
         xk = keys.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
         xv = values.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
 
-        output = self.sdpa(xq, xk, xv)
+        assert (
+            isinstance(attention_masks, BlockMask) or attention_masks is None
+        ), attention_masks
+
+        if self.use_flex_attn:
+            assert isinstance(attention_masks, BlockMask), attention_masks
+            output = self.inner_attention(xq, xk, xv, block_mask=attention_masks)
+        else:
+            assert attention_masks is None
+            output = self.inner_attention(xq, xk, xv)
 
         output = output.transpose(
             1, 2
@@ -321,6 +345,7 @@ class TransformerBlock(nn.Module):
         self,
         x: torch.Tensor,
         freqs_cis: torch.Tensor,
+        attention_masks: AttentionMasksType | None,
     ):
         """
         Perform a forward pass through the TransformerBlock.
@@ -333,7 +358,7 @@ class TransformerBlock(nn.Module):
             torch.Tensor: Output tensor after applying attention and feedforward layers.
 
         """
-        h = x + self.attention(self.attention_norm(x), freqs_cis)
+        h = x + self.attention(self.attention_norm(x), freqs_cis, attention_masks)
         out = h + self.feed_forward(self.ffn_norm(h))
         return out
 
@@ -428,9 +453,31 @@ class Transformer(nn.Module, ModelProtocol):
             self.model_args.rope_scaling_args,
         )
 
+    def get_attention_masks(
+        self,
+        input_batch: torch.Tensor,
+        tokenizer: BaseTokenizer,
+        extra_inputs: dict[str, torch.Tensor] | None = None,
+    ) -> AttentionMasksType:
+        mask_mods = [get_causal_mask_mod()]
+        match self.model_args.attn_mask_type:
+            case "causal":
+                B = 1
+            case "block_causal":
+                B = input_batch.shape[0]
+                mask_mods.append(get_document_mask_mod(input_batch, tokenizer.eos_id))
+            case _:
+                raise ValueError(
+                    f"Unknown attention mask type: {self.model_args.attn_mask_type}"
+                )
+        return create_attention_mask(
+            and_masks(*mask_mods), B, None, input_batch.shape[1], input_batch.shape[1]
+        )
+
     def forward(
         self,
         tokens: torch.Tensor,
+        attention_masks: AttentionMasksType | None = None,
         input_batch: torch.Tensor | None = None,
     ):
         """
@@ -454,7 +501,7 @@ class Transformer(nn.Module, ModelProtocol):
         h = self.tok_embeddings(tokens) if self.tok_embeddings else tokens
 
         for layer in self.layers.values():
-            h = layer(h, self.freqs_cis)
+            h = layer(h, self.freqs_cis, attention_masks=attention_masks)
 
         h = self.norm(h) if self.norm else h
         output = self.output(h) if self.output else h
