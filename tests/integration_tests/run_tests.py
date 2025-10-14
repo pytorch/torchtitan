108cf60d95680edbfc33d0f1144a092700b680c5
Akash Verma <Akash.Verma3@amd.com>
2025-10-14 14:23:43 -0500

    Skipping 2D eager test for rocm.

--- tests/integration_tests/run_tests.py
+++ tests/integration_tests/run_tests.py
@@ -27,6 +27,7 @@ _TEST_SUITES_FUNCTION = {
 # tests skipped for ROCm
 skip_for_rocm_test_list = [
     "model_only_hf_checkpoint",
+    "2D eager",
 ]
 TEST_WITH_ROCM = os.getenv("TEST_WITH_ROCM", "0") == "1"
 
