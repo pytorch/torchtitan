8f70bbfb8b63a3a7e6a32bdf4386750a549dbf1e
Tianyu Liu <lty@fb.com>
2025-10-13 20:42:03 -0700

    fix test

--- torchtitan/models/llama3_ft/__init__.py
+++ torchtitan/models/llama3_ft/__init__.py
@@ -11,20 +11,19 @@ from torchtitan.components.optimizer import build_optimizers
 from torchtitan.components.tokenizer import build_hf_tokenizer
 from torchtitan.components.validate import build_validator
 from torchtitan.datasets.hf_datasets import build_hf_dataloader
+from torchtitan.distributed.pipeline_parallel import pipeline_llm
 from torchtitan.protocols.train_spec import TrainSpec
 
 from ..llama3 import (
     llama3_configs,
     Llama3StateDictAdapter,
     parallelize_llama,
-    pipeline_llama,
     Transformer,
     TransformerModelArgs,
 )
 
 __all__ = [
     "parallelize_llama",
-    "pipeline_llama",
     "TransformerModelArgs",
     "Transformer",
     "llama3_configs",
@@ -36,7 +35,7 @@ def get_train_spec() -> TrainSpec:
         model_cls=Transformer,
         model_args=llama3_configs,
         parallelize_fn=parallelize_llama,
-        pipelining_fn=pipeline_llama,
+        pipelining_fn=pipeline_llm,
         build_optimizers_fn=build_optimizers,
         build_lr_schedulers_fn=build_lr_schedulers,
         build_dataloader_fn=build_hf_dataloader,
