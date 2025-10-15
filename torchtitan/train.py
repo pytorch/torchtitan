9e1d68eff3b11ca0f5957b9db6672dfcc0ac6c76
Chien-Chin Huang <chienchin@fb.com>
2025-10-15 11:35:21 -0700

    lint
    ghstack-source-id: 065ffd4952bdecef2bd5fb297cdaa7c844f502fe
    Pull-Request: https://github.com/pytorch/torchtitan/pull/1890

--- torchtitan/train.py
+++ torchtitan/train.py
@@ -11,9 +11,9 @@ from datetime import timedelta
 from typing import Any, Generator, Iterable, Optional
 
 import torch
+from torch.distributed.elastic.multiprocessing.errors import record
 
 import torchtitan.protocols.train_spec as train_spec_module
-from torch.distributed.elastic.multiprocessing.errors import record
 from torchtitan.components.checkpoint import CheckpointManager
 from torchtitan.components.dataloader import DataloaderExhaustedError
 from torchtitan.components.ft import FTManager, maybe_semi_sync_training
