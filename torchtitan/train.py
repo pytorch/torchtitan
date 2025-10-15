e58e44a6869dcc65e09311b1bedb719cba8a62ad
Chien-Chin Huang <chienchin@fb.com>
2025-10-15 11:35:00 -0700

    Update (base update)
    [ghstack-poisoned]

--- torchtitan/train.py
+++ torchtitan/train.py
@@ -11,9 +11,9 @@ from datetime import timedelta
 from typing import Any, Generator, Iterable, Optional
 
 import torch
-from torch.distributed.elastic.multiprocessing.errors import record
 
 import torchtitan.protocols.train_spec as train_spec_module
+from torch.distributed.elastic.multiprocessing.errors import record
 from torchtitan.components.checkpoint import CheckpointManager
 from torchtitan.components.dataloader import DataloaderExhaustedError
 from torchtitan.components.ft import FTManager, maybe_semi_sync_training
@@ -115,12 +115,14 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
 
         # Set random seed, and maybe enable deterministic mode
         # (mainly for debugging, expect perf loss).
+        """
         dist_utils.set_determinism(
-            world_mesh,
+            world_mesh._world_mesh,
             self.device,
             job_config.training.seed,
             job_config.training.deterministic,
         )
+        """
         self.train_spec = train_spec_module.get_train_spec(job_config.model.name)
 
         # build tokenizer and dataloader
@@ -639,7 +641,7 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
                         timeout=timedelta(
                             seconds=job_config.comm.train_timeout_seconds
                         ),
-                        world_mesh=self.parallel_dims.world_mesh,
+                        world_mesh=self.parallel_dims._world_mesh,
                     )
 
         if torch.distributed.get_rank() == 0:
