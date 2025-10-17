24c9e8666b5a2bfb57e7d9345745775cdb11cd8a
Jiani Wang <jianiw@meta.com>
2025-10-16 23:01:16 -0700

    refactor 1

--- .ci/docker/common/install_conda.sh
+++ .ci/docker/common/install_conda.sh
@@ -41,6 +41,7 @@ install_pip_dependencies() {
   # Install all Python dependencies
   pip_install -r /opt/conda/requirements-dev.txt
   pip_install -r /opt/conda/requirements.txt
+  pip_install -r /opt/conda/requirements-flux.txt
   pip_install -r /opt/conda/requirements-vlm.txt
   popd
 }
