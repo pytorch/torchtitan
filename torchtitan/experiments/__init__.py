fc46d960f75af7e7ee12a2bd7e8d395f9ec8bc59
Sherlock Huang <baihan.huang@gmail.com>
2025-10-16 21:40:27 -0700

    add joint graph runner deepseek_v3 experiment
    [ghstack-poisoned]

--- torchtitan/experiments/__init__.py
+++ torchtitan/experiments/__init__.py
@@ -5,5 +5,11 @@
 # LICENSE file in the root directory of this source tree.
 
 _supported_experiments = frozenset(
-    ["flux", "simple_fsdp.llama3", "simple_fsdp.deepseek_v3", "vlm"]
+    [
+        "flux",
+        "simple_fsdp.llama3",
+        "simple_fsdp.deepseek_v3",
+        "vlm",
+        "joint_graph_runner.deepseek_v3",
+    ]
 )
