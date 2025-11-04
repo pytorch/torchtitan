8a23f6ee2956bc8f453b1522e248d8d1b1841a52
Achazwl <323163497@qq.com>
2025-11-04 10:32:13 +0800

    fix lint

--- torchtitan/models/qwen3/model/state_dict_adapter.py
+++ torchtitan/models/qwen3/model/state_dict_adapter.py
@@ -120,9 +120,14 @@ class Qwen3StateDictAdapter(MoEStateDictAdapter):
         state_dict = {}
         expert_weights_by_layer = {}  # {layer: {abstract_key: {expert_id: tensor}}}
 
-        if self.model_args.enable_weight_tying and "lm_head.weight" not in hf_state_dict:
+        if (
+            self.model_args.enable_weight_tying
+            and "lm_head.weight" not in hf_state_dict
+        ):
             if "model.embed_tokens.weight" in hf_state_dict:
-                hf_state_dict["lm_head.weight"] = hf_state_dict["model.embed_tokens.weight"]
+                hf_state_dict["lm_head.weight"] = hf_state_dict[
+                    "model.embed_tokens.weight"
+                ]
 
         for key, value in hf_state_dict.items():
             if "mlp.experts" in key:
