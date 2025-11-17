b2b2b4f43a75e4da49798887d65a35d54700c605
Boyuan Feng <boyuan@meta.com>
2025-11-17 13:22:22 -0800

    cleanup

--- torchtitan/models/deepseek_v3/__init__.py
+++ torchtitan/models/deepseek_v3/__init__.py
@@ -97,7 +97,7 @@ deepseekv3_args = {
         qk_rope_head_dim=64,
         v_head_dim=128,
         mscale=0.70,
-        use_flex_attn=False,
+        use_flex_attn=True,
         attn_mask_type="block_causal",
     ),
     "236B": DeepSeekV3ModelArgs(
