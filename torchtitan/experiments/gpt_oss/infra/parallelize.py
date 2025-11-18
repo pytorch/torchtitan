7a628a74c285001d88999d0616de68d45351d8e0
Chien-Chin Huang <chienchin@fb.com>
2025-11-17 17:51:44 -0800

    fix comments

--- torchtitan/experiments/gpt_oss/infra/parallelize.py
+++ torchtitan/experiments/gpt_oss/infra/parallelize.py
@@ -121,18 +121,18 @@ def parallelize_gptoss(
     dp_mesh: DeviceMesh | None = None
     if parallel_dims.fsdp_enabled or parallel_dims.ep_enabled:
         # apply FSDP or HSDP, potentially with Context Parallel
-        names = (
+        dp_mesh_names = (
             ["dp_replicate", "fsdp"] if parallel_dims.dp_replicate_enabled else ["fsdp"]
         )
-        dp_mesh = parallel_dims.get_mesh(names)
+        dp_mesh = parallel_dims.get_mesh(dp_mesh_names)
 
         # the mesh dim names of which the MoE params are sharded on via FSDP/HSDP
-        edp_mesh = None
-        if parallel_dims.ep_enabled:
-            if parallel_dims.dp_replicate_enabled:
-                edp_mesh = parallel_dims.get_mesh(["dp_replicate", "efsdp"])
-            else:
-                edp_mesh = parallel_dims.get_mesh("efsdp")
+        edp_mesh_names = (
+            ["dp_replicate", "efsdp"]
+            if parallel_dims.dp_replicate_enabled
+            else ["efsdp"]
+        )
+        edp_mesh = parallel_dims.get_mesh(edp_mesh_names)
 
         apply_fsdp(
             model,
