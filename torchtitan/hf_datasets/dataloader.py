434a2b984037017146b3573b4a1074ae9d7a698b
dmahan93 <44207705+dmahan93@users.noreply.github.com>
2025-11-19 07:58:57 -0800

    Add packed memmap dataset (#18)
    * add packed memmap dataset
    
    * add pretokenizer and example processing script
    
    * add pretokenizer and example processing script

--- torchtitan/hf_datasets/dataloader.py
+++ torchtitan/hf_datasets/dataloader.py
@@ -1,14 +1,16 @@
-import torchtitan
 import importlib
+
 from torchtitan.components.tokenizer import Tokenizer
 from torchtitan.config import JobConfig
+from .packed_memmap import build_memmap_text_dataloader
+from .preprocessed import build_preprocessed_dataloader
 
 from .text_datasets import build_text_dataloader
-from .preprocessed import build_preprocessed_dataloader
 
 DATALOADERS = {
     "huggingface": build_text_dataloader,
     "preprocessed": build_preprocessed_dataloader,
+    "packed_memmap": build_memmap_text_dataloader,
 }
 
 
@@ -24,9 +26,10 @@ def build_dataloader(
     if job_config.training.dataset_type == "nanoset":
         if importlib.util.find_spec("datatrove") is not None:
             from .nanoset import build_nanoset_dataloader
+
             dataloader = build_nanoset_dataloader
         else:
-            raise RuntimeError(f"Install `datatrove` package for nanoset support")
+            raise RuntimeError("Install `datatrove` package for nanoset support")
     elif job_config.training.dataset_type not in DATALOADERS:
         raise ValueError(f"Unknown dataset type `{job_config.training.dataset_type}`")
     else:
