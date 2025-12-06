0a9815be423ff6f4f990b85db345436cbc8a6eff
Elfie Guo <elfieg@nvidia.com>
2025-12-05 23:16:56 -0800

    Add deepep folder..

---
+++ torchtitan/distributed/deepep/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""DeepEP distributed communication primitives for MoE."""
+
+from .flex_dispatcher import MoEFlexTokenDispatcher
+
+__all__ = [
+    "MoEFlexTokenDispatcher",
+]
+
