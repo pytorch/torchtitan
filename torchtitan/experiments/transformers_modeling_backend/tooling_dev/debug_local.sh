a54016b46e69ea56a5a4f3198d093865fafa8407
3outeille <ferdinand.mom@epita.fr>
2025-12-15 13:30:03 +0000

    Enhance debug_local.sh to support compile flag

--- torchtitan/experiments/transformers_modeling_backend/tooling_dev/debug_local.sh
+++ torchtitan/experiments/transformers_modeling_backend/tooling_dev/debug_local.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+# Parse command line arguments
+COMPILE_FLAG=""
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --compile)
+            COMPILE_FLAG="--enable_compile"
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            echo "Usage: $0 [--compile]"
+            exit 1
+            ;;
+    esac
+done
+
 # create a list of model_name
 
 model_names=(
@@ -24,7 +40,7 @@ model_names=(
 for model_name in "${model_names[@]}"; do
     rm -rf debug_local_results/${model_name}
 
-    python ./tooling_dev/test_hf_integration.py create_configs --model_name "$model_name" --out_dir debug_local_results --flavor debugmodel
+    python ./tooling_dev/test_hf_integration.py create_configs --model_name "$model_name" --out_dir debug_local_results --flavor debugmodel $COMPILE_FLAG
     python ./tooling_dev/test_hf_integration.py submit_jobs --inp_dir debug_local_results/${model_name}/debugmodel/seed_checkpoint --qos high
     while [ ! -f debug_local_results/${model_name}/debugmodel/seed_checkpoint/status.txt ] || [ "$(cat debug_local_results/${model_name}/debugmodel/seed_checkpoint/status.txt)" != "completed" ]; do
         echo "Waiting for seed checkpoint from ${model_name} to complete ..."
@@ -37,7 +53,7 @@ done
 # for model_name in "${moe_model_names[@]}"; do
 #     rm -rf debug_local_results/${model_name}
 
-#     USE_MOE=1 python ./tooling_dev/test_hf_integration.py create_configs --model_name "$model_name" --out_dir debug_local_results --flavor debugmodel
+#     USE_MOE=1 python ./tooling_dev/test_hf_integration.py create_configs --model_name "$model_name" --out_dir debug_local_results --flavor debugmodel $COMPILE_FLAG
 #     USE_MOE=1 python ./tooling_dev/test_hf_integration.py submit_jobs --inp_dir debug_local_results/${model_name}/debugmodel/seed_checkpoint --qos high
 #     while [ ! -f debug_local_results/${model_name}/debugmodel/seed_checkpoint/status.txt ] || [ "$(cat debug_local_results/${model_name}/debugmodel/seed_checkpoint/status.txt)" != "completed" ]; do
 #         echo "Waiting for seed checkpoint from ${model_name} to complete ..."
