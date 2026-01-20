d7a194b212695a00a8d622a75b55f24775cb1562
mori360 <yifanmao@meta.com>
2026-01-20 13:35:20 -0800

    clean

--- torchtitan/train.py
+++ torchtitan/train.py
@@ -146,7 +146,7 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
             f"with {json.dumps(dataclasses.asdict(model_args), indent=2, ensure_ascii=False)}"
         )
         with (
-            torch.device("cuda"),
+            torch.device("meta"),
             utils.set_default_dtype(TORCH_DTYPE_MAP[job_config.training.dtype]),
         ):
             model = self.train_spec.model_cls(model_args)
