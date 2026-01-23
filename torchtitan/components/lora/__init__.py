b541bcbc524eb396afd4305e42729354bdf110eb
mori360 <yifanmao@meta.com>
2026-01-22 16:00:50 -0800

    lint

--- torchtitan/components/lora/__init__.py
+++ torchtitan/components/lora/__init__.py
@@ -4,10 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torchtitan.components.lora.lora import (
-    LoRAConverter,
-    LoRALinear,
-)
+from torchtitan.components.lora.lora import LoRAConverter, LoRALinear
 
 __all__ = [
     "LoRAConverter",
