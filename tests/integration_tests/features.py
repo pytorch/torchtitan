02661e89427aa7ced5eee2c29041e0e27b9de111
akashveramd <Akash.Verma3@amd.com>
2026-02-01 22:58:05 -0800

    Add ROCm CI support for Auto Parallel & Compiler Toolkit experiments (#2248)
    In this PR, we are adding ROCm CI support for Auto Parallel & Compiler
    Toolkit experiments.
    Also, enabling test_generate features test for ROCm.

--- tests/integration_tests/features.py
+++ tests/integration_tests/features.py
@@ -484,7 +484,6 @@ def build_features_test_list() -> list[OverrideDefinitions]:
             "Generation script test",
             "test_generate",
             ngpu=2,
-            skip_rocm_test=True,
         ),
         OverrideDefinitions(
             [
