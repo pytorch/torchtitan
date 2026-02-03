5928f10c59f2ea7456d5089657ef2d1aafd8a4fe
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2026-02-03 12:51:21 +0400

    add comment

--- torchtitan/experiments/simple_fsdp/simple_fsdp.py
+++ torchtitan/experiments/simple_fsdp/simple_fsdp.py
@@ -349,20 +349,35 @@ def data_parallel(
     return model
 
 
-def _register_placement_as_opaque():
-    from torch._library.opaque_object import MemberType, register_opaque_type
+def _maybe_register_placement_as_opaque():
+    # The PyTorch PR https://github.com/pytorch/pytorch/pull/171482
+    # has removed PlacementVariable, so now TorchDynamo requires an opaque type
+    # registration for placements and derived objects like `_ScaledPartial`.
+    # According to https://github.com/pytorch/pytorch/blob/main/torch/_library/opaque_object.py
+    # Opaque objects are the way TorchDynamo allows custom operators to accept
+    # a user-defined "black box" object as an input. Users can register a custom class as:
+    #    register_opaque_type(MyClass, typ=..., members=...)
+    # where `typ` is either "reference" or "value", and `members` is a dictionary mapping
+    # member names (attributes, properties, or methods) to their MemberType, which controls
+    # how they are handled during torch.compile tracing
 
-    allowed_members = {
-        "reduce_op": MemberType.USE_REAL,
-        "is_shard": MemberType.USE_REAL,
-        "is_partial": MemberType.USE_REAL,
-        "is_replicate": MemberType.USE_REAL,
-        "__eq__": MemberType.USE_REAL,
-    }
-    register_opaque_type(
-        _ScaledPartial,
-        typ="value",
-        members=allowed_members,
-    )
+    from torch._dynamo import variables
 
-_register_placement_as_opaque()
+    if not hasattr("PlacementVariable", variables):
+        from torch._library.opaque_object import MemberType, register_opaque_type
+
+        allowed_members = {
+            "reduce_op": MemberType.USE_REAL,
+            "is_shard": MemberType.USE_REAL,
+            "is_partial": MemberType.USE_REAL,
+            "is_replicate": MemberType.USE_REAL,
+            "__eq__": MemberType.USE_REAL,
+        }
+        register_opaque_type(
+            _ScaledPartial,
+            typ="value",
+            members=allowed_members,
+        )
+
+
+_maybe_register_placement_as_opaque()
