6bb75c9e47f217af0bf0e92d7042b879f6246518
zhxchen17 <zhxchen17@fb.com>
2026-02-04 11:58:05 -0800

    [rl][ez] Fix import errors.
    Summary:
    
    I'm seeing some import errors while rebasing on the latest vllm and torchtitan. Fixing these.
    
    Test Plan:
    python torchtitan/experiments/rl/unified/infer.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 04f049f04b1f8884a013baa92de3ed8537ca6b13
    Pull Request resolved: https://github.com/pytorch/torchtitan/pull/2324

--- torchtitan/experiments/rl/unified/models/attention.py
+++ torchtitan/experiments/rl/unified/models/attention.py
@@ -5,7 +5,11 @@
 # LICENSE file in the root directory of this source tree.
 
 import torch
-from vllm.attention.layer import Attention
+
+try:
+    from vllm.attention.layer import Attention
+except ModuleNotFoundError:
+    from vllm.model_executor.layers.attention import Attention
 
 
 class VLLMAttention(torch.nn.Module):
