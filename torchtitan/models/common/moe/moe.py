adcf05220d10252f0ae426c5cfa58d294154c9ef
Chelsea Lowman <clowman1993@gmail.com>
2026-02-27 17:46:19 -0500

    Remove unnecessary float32 cast

--- torchtitan/models/common/moe/moe.py
+++ torchtitan/models/common/moe/moe.py
@@ -260,10 +260,11 @@ class TokenChoiceTopKRouter(nn.Module):
             scores = self.gate(x)
 
         # By default, sigmoid or softmax is performed in float32 to avoid loss explosion
+        # scored is already float32 from the autocast above. 
         if self.score_func == "sigmoid":
-            scores = torch.sigmoid(scores.to(torch.float32))
+            scores = torch.sigmoid(scores)
         elif self.score_func == "softmax":
-            scores = F.softmax(scores.to(torch.float32), dim=1)
+            scores = F.softmax(scores, dim=1)
         else:
             raise NotImplementedError(f"Unknown score function {self.score_func}")
 
