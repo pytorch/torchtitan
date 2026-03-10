63801e0b8df4d65313fbaf0833d31d1a51e6da65
mori360 <yifanmao@meta.com>
2026-03-10 13:44:32 -0700

    update

--- tests/integration_tests/features.py
+++ tests/integration_tests/features.py
@@ -42,13 +42,13 @@ def build_features_test_list() -> list[OverrideDefinitions]:
             [
                 [
                     "--comm.use_torchcomms",
-                    "--parallelism.tensor_parallel_degree 2",
                     "--parallelism.context_parallel_degree 2",
+                    "--parallelism.pipeline_parallel_degree 2",
                     "--compile.enable",
                 ],
             ],
-            "FSDP+TP+CP+compile with torchcomms",
-            "torchcomms_3d_dp+tp+cp+compile",
+            "FSDP+CP+PP+compile with torchcomms",
+            "torchcomms_3d_dp+cp+pp+compile",
             ngpu=8,
         ),
         OverrideDefinitions(
