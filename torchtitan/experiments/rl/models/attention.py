938ae5d21ae373d29859a150e59674bf5f79546b
Zhengxu Chen <zhxchen17@meta.com>
2026-04-07 15:51:33 -0400

    [rl][ez] Add comments for FA kernel activation. (#2870)
    Adding some comments to clarify why we are checking against current
    activated FA kernel impl.

--- torchtitan/experiments/rl/models/attention.py
+++ torchtitan/experiments/rl/models/attention.py
@@ -65,6 +65,8 @@ class PyTorchVarlenAttentionImpl(FlashAttentionImpl):
 
         # Hopper (SM 9.0) uses FA3
         if has_cuda_capability(9, 0):
+            # activate_flash_attention_impl() will restore internal global state
+            # and re-run register function, so we want to only call it once.
             if current_flash_attention_impl() != "FA3":
                 activate_flash_attention_impl("FA3")
         else:
@@ -168,6 +170,9 @@ class PyTorchVarlenAttentionImpl(FlashAttentionImpl):
         # split-k reductions. FA2 is automatically batch-invariant and does
         # not accept num_splits.
         extra_kwargs = {}
+
+        # Disable split_kv in Flash Attention to ensure bitwise identical output.
+        # see https://github.com/pytorch/pytorch/pull/176905
         if is_in_batch_invariant_mode() and current_flash_attention_impl() == "FA3":
             extra_kwargs["num_splits"] = 1
 
