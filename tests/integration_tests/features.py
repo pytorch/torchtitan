0443302ebf60d67ce4465d9ea7b482e9eae6e3b4
mori360 <yifanmao@meta.com>
2026-05-08 16:11:29 -0700

    merge lora and float8 tests

--- tests/integration_tests/features.py
+++ tests/integration_tests/features.py
@@ -577,22 +577,13 @@ def build_features_test_list() -> list[OverrideDefinitions]:
         OverrideDefinitions(
             [
                 [
-                    "--module llama3 --config llama3_debugmodel_float8_emulate",
-                ],
-            ],
-            "Float8 emulation test",
-            "float8_emulation",
-        ),
-        OverrideDefinitions(
-            [
-                [
                     "--module llama3 --config llama3_debugmodel_lora",
                     "--parallelism.tensor_parallel_degree 2",
                     "--parallelism.pipeline_parallel_degree 2",
                 ],
             ],
-            "LoRA training test",
-            "lora",
+            "Float8 emulate + LoRA training test",
+            "float8_emulate_lora",
             ngpu=8,
         ),
         OverrideDefinitions(
