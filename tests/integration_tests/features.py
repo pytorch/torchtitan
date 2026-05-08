6f2d05b0e0ed99e405ed07ef2d93138967aa335c
mori360 <yifanmao@meta.com>
2026-05-07 17:20:43 -0700

    update

--- tests/integration_tests/features.py
+++ tests/integration_tests/features.py
@@ -587,6 +587,7 @@ def build_features_test_list() -> list[OverrideDefinitions]:
             [
                 [
                     "--module llama3 --config llama3_debugmodel_lora",
+                    "--parallelism.tensor_parallel_degree 2",
                 ],
             ],
             "LoRA training test",
