d87fe9338fc6724f23646ea9b5e5cd740ad2fac8
Wei Feng <weif@meta.com>
2026-05-08 09:21:25 -0700

    Update (base update)
    [ghstack-poisoned]

---
+++ torchtitan/experiments/flex_shard/__init__.py
@@ -0,0 +1,64 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .flex_shard import (
+    auto_buckets,
+    BucketSpec,
+    disable_active_parametrization,
+    DStorage,
+    flat_shard_placements,
+    FlatShard,
+    FlatShardParametrization,
+    flex_shard,
+    FlexShardMeshInfo,
+    FlexShardModule,
+    get_global_shape,
+    get_placements,
+    is_flex_shard_param,
+    lift_params_to_global_spmd_mesh,
+    MixedPrecisionPolicy,
+    OffloadPolicy,
+    Owned,
+    OwnedParametrization,
+    param_boundary_placements,
+    per_param_placements,
+    Placement,
+    RaggedShard,
+    RaggedShardParametrization,
+    set_sharding_info,
+    Shard,
+    ShardParametrization,
+)
+
+
+__all__ = [
+    "auto_buckets",
+    "BucketSpec",
+    "disable_active_parametrization",
+    "DStorage",
+    "FlexShardMeshInfo",
+    "flat_shard_placements",
+    "FlatShard",
+    "FlatShardParametrization",
+    "flex_shard",
+    "FlexShardModule",
+    "get_global_shape",
+    "get_placements",
+    "is_flex_shard_param",
+    "lift_params_to_global_spmd_mesh",
+    "MixedPrecisionPolicy",
+    "OffloadPolicy",
+    "Owned",
+    "OwnedParametrization",
+    "param_boundary_placements",
+    "per_param_placements",
+    "Placement",
+    "RaggedShard",
+    "RaggedShardParametrization",
+    "set_sharding_info",
+    "Shard",
+    "ShardParametrization",
+]
