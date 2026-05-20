d44fd222ec43d18912baee4d3c280c7407695218
Aditya Venkataraman <aditya.venky@gmail.com>
2026-05-19 22:33:42 -0700

    Try FSDP symmetric memory comm

--- torchtitan/models/qwen3/parallelize.py
+++ torchtitan/models/qwen3/parallelize.py
@@ -89,6 +89,8 @@ def parallelize_qwen3(
         fully_shard(layer, **fsdp_config)
     fully_shard(model.lm_head, **fsdp_config)
     fully_shard(model, **fsdp_config)
+    for fsdp_module in [*layers, model.lm_head, model]:
+        fsdp_module.set_symm_mem_for_comm("NCCL")
 
     if layers:
         for layer, next_layer in zip(layers, layers[1:]):
