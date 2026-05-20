ad3a5aafe5466744df7760aa10559338ff03d59b
Sherlock Huang <baihan.huang@gmail.com>
2026-05-19 18:00:28 -0700

    Remove FUSED_KERNELS shortcut; use FUSED_KERNEL_DIR only

--- run_graph_trainer_llama3_8b.sh
+++ run_graph_trainer_llama3_8b.sh
@@ -50,8 +50,6 @@ tlp ()
 FUSED_KERNEL_FLAGS=""
 if [ -n "${FUSED_KERNEL_DIR:-}" ]; then
     FUSED_KERNEL_FLAGS="--compile.fused_kernel_dir $FUSED_KERNEL_DIR"
-elif [ "${FUSED_KERNELS:-0}" = "1" ]; then
-    FUSED_KERNEL_FLAGS="--compile.fused_kernel_dir torchtitan/experiments/graph_trainer/kernel_gen/generated"
 fi
 
 export PYTORCH_ALLOC_CONF="expandable_segments:True"
