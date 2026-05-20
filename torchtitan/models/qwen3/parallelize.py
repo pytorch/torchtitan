b97a2815976c7cf4e392cfaf742b0861b18bf6d5
Aditya Venkataraman <aditya.venky@gmail.com>
2026-05-20 04:05:44 -0700

    Add embedding FSDP prefetch candidate

--- torchtitan/models/qwen3/parallelize.py
+++ torchtitan/models/qwen3/parallelize.py
@@ -85,17 +85,20 @@ def parallelize_qwen3(
     }
 
     layers = list(model.layers.values())
+    fully_shard(model.tok_embeddings, **fsdp_config)
     for layer in layers:
         fully_shard(layer, **fsdp_config)
     fully_shard(model.lm_head, **fsdp_config)
     fully_shard(model, **fsdp_config)
     if layers:
+        model.tok_embeddings.set_modules_to_forward_prefetch([layers[0]])
         for layer, next_layer in zip(layers, layers[1:]):
             layer.set_modules_to_forward_prefetch([next_layer])
         layers[-1].set_modules_to_forward_prefetch([model.lm_head])
         model.lm_head.set_modules_to_backward_prefetch([layers[-1]])
         for layer, prev_layer in zip(reversed(layers[1:]), reversed(layers[:-1])):
             layer.set_modules_to_backward_prefetch([prev_layer])
+        layers[0].set_modules_to_backward_prefetch([model.tok_embeddings])
 
     disable_fsdp_gradient_division(model)
     logger.info(
