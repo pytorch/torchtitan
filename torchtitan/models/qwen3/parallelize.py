53ac215a2bed679c710208bafb566593a39c2504
Aditya Venkataraman <aditya.venky@gmail.com>
2026-05-19 23:08:39 -0700

    Try root endpoint FSDP prefetch

--- torchtitan/models/qwen3/parallelize.py
+++ torchtitan/models/qwen3/parallelize.py
@@ -90,12 +90,14 @@ def parallelize_qwen3(
     fully_shard(model.lm_head, **fsdp_config)
     fully_shard(model, **fsdp_config)
     if layers:
+        model.set_modules_to_forward_prefetch([layers[0]])
         for layer, next_layer in zip(layers, layers[1:]):
             layer.set_modules_to_forward_prefetch([next_layer])
         layers[-1].set_modules_to_forward_prefetch([model.lm_head])
         model.lm_head.set_modules_to_backward_prefetch([layers[-1]])
         for layer, prev_layer in zip(reversed(layers[1:]), reversed(layers[:-1])):
             layer.set_modules_to_backward_prefetch([prev_layer])
+        layers[0].set_modules_to_backward_prefetch([model])
 
     disable_fsdp_gradient_division(model)
     logger.info(
