00f598f48a0869aa0e4733e255ee476e3f1b9e70
Anshul Sinha <anshulsi@meta.com>
2026-05-21 13:23:37 -0700

    Update (base update)
    [ghstack-poisoned]

---
+++ torchtitan/experiments/flex_shard/flex_shard/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .bucket_storage import BucketSpec, MixedPrecisionPolicy, OffloadPolicy, PlacementFn
+from .flex_shard import flex_shard
+from .placement_contract import LocalStorageLayout, Placement
+from .sharded_param import get_global_shape, get_placements, is_flex_shard_param
+
+__all__ = [
+    "BucketSpec",
+    "flex_shard",
+    "get_global_shape",
+    "get_placements",
+    "is_flex_shard_param",
+    "LocalStorageLayout",
+    "MixedPrecisionPolicy",
+    "OffloadPolicy",
+    "Placement",
+    "PlacementFn",
+]
