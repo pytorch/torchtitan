e7429d5a18a4eccb7b0333f2d67b5171bd77f460
Aditya Venkataraman <aditya.venky@gmail.com>
2026-05-22 14:24:31 -0700

    Restore Qwen3 per-layer FSDP groups

--- torchtitan/models/qwen3/parallelize.py
+++ torchtitan/models/qwen3/parallelize.py
@@ -85,8 +85,8 @@ def parallelize_qwen3(
     }
 
     layers = list(model.layers.values())
-    for i in range(0, len(layers), 3):
-        fully_shard(layers[i : i + 3], **fsdp_config)
+    for layer in layers:
+        fully_shard(layer, **fsdp_config)
     fully_shard(model.lm_head, **fsdp_config)
     fully_shard(model, **fsdp_config)
     if layers:
