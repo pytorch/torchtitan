f58e1814c72b2010e7f7cc4dd0b21054ae969ed2
Aksionau Andrei <aksionau.andrei@gmail.com>
2026-05-29 14:13:31 +0300

    Update docstring for `exclude_from_loading`

--- torchtitan/components/checkpoint.py
+++ torchtitan/components/checkpoint.py
@@ -322,8 +322,8 @@ class CheckpointManager(Configurable):
 
         exclude_from_loading: list[str] = field(default_factory=list)
         """
-        Exclude specific keys from being loaded from the checkpoint.
-        Keys shouldn't include "model" key.
+        Exclude specific keys from being loaded from the checkpoint (e.g. 'optimizer',
+        `lr_scheduler`, `dataloader'). Keys shouldn't include 'model' key.
         """
 
         enable_first_step_checkpoint: bool = False
