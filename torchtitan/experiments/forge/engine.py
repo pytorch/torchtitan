c2577d11e3da9f0c838f6f82af4a394e952efeff
Pian Pawakapan <pianpwk@meta.com>
2026-06-16 16:31:25 -0700

    Update (base update)
    [ghstack-poisoned]

--- torchtitan/experiments/forge/engine.py
+++ torchtitan/experiments/forge/engine.py
@@ -283,9 +283,8 @@ class ForgeEngine(torch.distributed.checkpoint.stateful.Stateful, Configurable):
             base_folder=config.dump_folder,
         )
 
-        loss_parallel_enabled = parallel_dims.tp_enabled
         self.train_context = dist_utils.get_train_context(
-            enable_loss_parallel=loss_parallel_enabled,
+            enable_loss_parallel=config.parallelism.tensor_parallel_degree > 1,
             parallel_dims=parallel_dims,
         )
 
