b14e83ed7abf5ccc127f19283da366c0a746bcd3
Wei Feng <weif@meta.com>
2026-06-22 15:59:55 -0700

    Update
    [ghstack-poisoned]

--- torchtitan/experiments/flex_shard/example/__init__.py
+++ torchtitan/experiments/flex_shard/example/__init__.py
@@ -5,23 +5,11 @@
 # LICENSE file in the root directory of this source tree.
 
 from .owned import Owned, param_boundary_placements
-from .ragged_shard import (
-    GroupedRaggedShard,
-    make_grouped_ragged_placement_fn,
-    make_ragged_placement_fn,
-    per_param_ragged_placements,
-    RaggedShard,
-)
 from .shard import per_param_placements, Shard
 
 __all__ = [
-    "GroupedRaggedShard",
-    "make_grouped_ragged_placement_fn",
-    "make_ragged_placement_fn",
     "Owned",
     "param_boundary_placements",
-    "per_param_ragged_placements",
     "per_param_placements",
-    "RaggedShard",
     "Shard",
 ]
