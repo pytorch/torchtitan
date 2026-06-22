eaf41589cab58591924ad3050a356b231762b963
Wei Feng <weif@meta.com>
2026-06-21 22:56:00 -0700

    Update
    [ghstack-poisoned]

--- torchtitan/experiments/flex_shard/deepseek_v3/config_registry.py
+++ torchtitan/experiments/flex_shard/deepseek_v3/config_registry.py
@@ -10,7 +10,7 @@ from torchtitan.trainer import Trainer
 from . import model_registry
 
 
-def flex_shard_deepseek_v3_16b_dp8() -> Trainer.Config:
+def flex_shard_deepseek_v3_16b() -> Trainer.Config:
     config = deepseek_v3_16b()
     config.model_spec = model_registry("16B", attn_backend="flex")
     return config
