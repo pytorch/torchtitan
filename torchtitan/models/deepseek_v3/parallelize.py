622c1acaa80b7ad7b3fcc4988fdb28375322375e
Pian Pawakapan <pianpwk@meta.com>
2026-06-26 00:15:50 -0700

    [spmd_types] dsv3 enablement (#3673)
    Stack from [ghstack](https://github.com/ezyang/ghstack/tree/0.12.0)
    (oldest at bottom):
    * #3740
    * #3690
    * __->__ #3673

--- torchtitan/models/deepseek_v3/parallelize.py
+++ torchtitan/models/deepseek_v3/parallelize.py
@@ -34,7 +34,7 @@ def parallelize_deepseekv3(
     ac_config: ActivationCheckpointingConfig,
     dump_folder: str,
 ):
-    if parallelism.spmd_backend == "full_dtensor":
+    if parallelism.spmd_backend in ("full_dtensor", "spmd_types"):
         validate_config(parallel_dims, model)
         model.parallelize(parallel_dims)
     else:
@@ -62,7 +62,7 @@ def parallelize_deepseekv3(
     if model_compile_enabled:
         apply_compile(model, compile_config)
 
-    if parallelism.spmd_backend == "full_dtensor":
+    if parallelism.spmd_backend in ("full_dtensor", "spmd_types"):
         dp_mesh, dp_mesh_dims = resolve_fsdp_mesh(parallel_dims)
         edp_mesh, edp_mesh_dims = resolve_sparse_fsdp_mesh(parallel_dims)
     else:
