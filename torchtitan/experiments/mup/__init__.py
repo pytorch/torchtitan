6cafe2fb5bccd84984f4226310f50fe5a1a002df
Utkarsh Gill <ugill@ee.iitr.ac.in>
2026-06-29 11:48:31 -0700

    mup: drop dead __init__ exports

--- torchtitan/experiments/mup/__init__.py
+++ torchtitan/experiments/mup/__init__.py
@@ -3,10 +3,3 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
-from .spec import MuPSweepSpec, SPECS
-
-__all__ = [
-    "MuPSweepSpec",
-    "SPECS",
-]
