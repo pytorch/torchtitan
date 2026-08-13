5d08d0435e6eebd3061c8155589830b87e0328c1
Wei Feng <weif@meta.com>
2026-08-12 20:17:58 -0700

    Update (base update)
    [ghstack-poisoned]

--- tests/unit_tests/flex_shard/test_distributed_muon.py
+++ tests/unit_tests/flex_shard/test_distributed_muon.py
@@ -24,7 +24,7 @@ from torchtitan.distributed.flex_shard import (
     build_distributed_muon,
     ComputeLayout,
     MuonComputeShardingConfig,
-    SingleParticipant,
+    Owned,
 )
 from torchtitan.distributed.flex_shard.distributed_muon import (
     _adjust_muon_learning_rate,
@@ -62,7 +62,7 @@ class TestDistributedMuon(DTensorTestBase):
             compute_sharding_by_fqn={
                 fqn: MuonComputeShardingConfig(
                     compute_layout=ComputeLayout(
-                        distribution_by_mesh_axis={
+                        shardings_by_mesh_axis={
                             "dp_shard": Replicate(),
                         },
                     )
@@ -136,14 +136,14 @@ class TestDistributedMuon(DTensorTestBase):
                 compute_sharding_by_fqn={
                     redistributed_fqn: MuonComputeShardingConfig(
                         compute_layout=ComputeLayout(
-                            distribution_by_mesh_axis={
-                                "dp_shard": SingleParticipant(),
+                            shardings_by_mesh_axis={
+                                "dp_shard": Owned(),
                             },
                         )
                     ),
                     local_blocks_fqn: MuonComputeShardingConfig(
                         compute_layout=ComputeLayout(
-                            distribution_by_mesh_axis={"dp_shard": Shard(0)},
+                            shardings_by_mesh_axis={"dp_shard": Shard(0)},
                         ),
                         compute_view=AttentionPerHeadComputeView(
                             num_heads=num_heads,
@@ -345,7 +345,7 @@ class TestDistributedMuonInitialExpertStorageContract(DTensorTestBase):
                 compute_sharding_by_fqn={
                     fqn: MuonComputeShardingConfig(
                         compute_layout=ComputeLayout(
-                            distribution_by_mesh_axis={
+                            shardings_by_mesh_axis={
                                 "efsdp": Shard(0),
                                 "ep": Shard(0),
                             },
