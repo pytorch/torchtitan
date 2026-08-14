95d3975e0afa64537d5f537654c80c9d23607364
Felipe Mello <felipemello@fb.com>
2026-08-14 03:17:17 -0700

    Test tensor logging with strict SPMD types

--- tests/integration_tests/features.py
+++ tests/integration_tests/features.py
@@ -168,6 +168,21 @@ def build_features_test_list() -> list[OverrideDefinitions]:
         OverrideDefinitions(
             [
                 [
+                    "--metrics.enable_tensorboard",
+                    "--metrics.log-freq 2",
+                    "--metrics.tensor-logging.enabled",
+                    "--metrics.tensor-logging.freq 2",
+                    "--training.steps 4",
+                    "--parallelism.tensor_parallel_degree 2",
+                ],
+            ],
+            "Tensor logging with strict SPMD typechecking",
+            "tensor_logging_spmd_typechecking",
+            ngpu=2,
+        ),
+        OverrideDefinitions(
+            [
+                [
                     "--compile.enable",
                 ],
             ],
