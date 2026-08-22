dee45e35721d24b8ab959a93140d0f799734eed1
JavaZeroo <2487163254@qq.com>
2026-08-22 10:12:26 +0000

    fix test_update_from_config_propagates_moe_force_load_balance when rebase

--- tests/unit_tests/test_kimi_k3.py
+++ tests/unit_tests/test_kimi_k3.py
@@ -148,12 +148,15 @@ class TestKimiK3(unittest.TestCase):
         self.assertIsInstance(attention_masks, BlockMask)
 
     def test_update_from_config_propagates_moe_force_load_balance(self):
+        from torchtitan.components.data import GrainDataLoader
         from torchtitan.config import DebugConfig
+        from torchtitan.hf_datasets.multimodal.mm_datasets import MM_DATASETS
         from torchtitan.trainer import Trainer
 
         model_config = _small_model_config()
         runtime_config = Trainer.Config(
             debug=DebugConfig(moe_force_load_balance=True),
+            dataloader=GrainDataLoader.Config(dataset=MM_DATASETS["cc12m-test"]),
             activation_checkpoint=None,
         )
         model_config.update_from_config(config=runtime_config)
