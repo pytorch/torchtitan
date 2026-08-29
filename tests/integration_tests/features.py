dfeb5e83c9002c2c1c32ea3b3ea27a79b6f274be
QIU023 <yiqiaoqiu@hotmail.com>
2026-08-29 17:30:24 +0000

    kimi_k3: the pp integration cells skip on ROCm

--- tests/integration_tests/features.py
+++ tests/integration_tests/features.py
@@ -309,6 +309,7 @@ def build_features_test_list() -> list[OverrideDefinitions]:
             test_name="kimi_k3_pp2",
             ngpu=2,
             use_real_pg=True,
+            skip_rocm_test=True,
         ),
         OverrideDefinitions(
             configs=[recipes.kimi_k3_debugmodel_pp8_vp4],
@@ -316,5 +317,6 @@ def build_features_test_list() -> list[OverrideDefinitions]:
             test_name="kimi_k3_pp8_vp4",
             ngpu=8,
             use_real_pg=True,
+            skip_rocm_test=True,
         ),
     ]
