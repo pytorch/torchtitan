ac7cbed3a9a6fab2f5097e1689eae3c993ada2af
Rishi Sinha <60558850+rishisinhanj@users.noreply.github.com>
2026-09-01 18:02:54 -0400

    [ROCm][CI] Guard HIPBLASLT_TENSILE_LIBPATH and disable expandable_segments (#7)

--- run_train.sh
+++ run_train.sh
@@ -38,7 +38,7 @@ if [ "$COMM_MODE" = "fake_backend" ]; then
     NGPU="${NGPU}" LOCAL_RANK=0 python3 -m torchtitan.train --module ${MODULE} --config ${CONFIG} --comm.mode=fake_backend --training.steps 1 "$@"
 else
     # Normal training with torchrun
-    PYTORCH_ALLOC_CONF="expandable_segments:True" \
+    PYTORCH_ALLOC_CONF="${PYTORCH_ALLOC_CONF:-expandable_segments:True}" \
     TORCHFT_LIGHTHOUSE=${TORCHFT_LIGHTHOUSE} \
     torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
     --local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
