a5e7922f48d42f990d4594e0b864745e468d2d38
Animesh Jain <anijain@umich.edu>
2026-09-10 16:08:53 -0700

    Update (base update)
    [ghstack-poisoned]

--- torchtitan/experiments/transformers_modeling_backend/tests/test_moe_parallelism.py
+++ torchtitan/experiments/transformers_modeling_backend/tests/test_moe_parallelism.py
@@ -26,7 +26,8 @@ from torchtitan.models.common.moe import MoE
 
 def _expert_weights(experts):
     """Return logical gate, down, and up expert-weight tensors."""
-    return experts.w13[:, :, 0, :], experts.w2_EDF, experts.w13[:, :, 1, :]
+    logical_w13 = experts.w13.unflatten(1, (-1, 2))
+    return logical_w13[:, :, 0, :], experts.w2_EDF, logical_w13[:, :, 1, :]
 
 
 def _moe_buffer(moe, prefix):
