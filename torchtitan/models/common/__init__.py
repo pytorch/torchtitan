5742a80776c65651e56064d7f461068e720e1fa8
Animesh Jain <anijain@umich.edu>
2026-09-09 18:33:43 -0700

    Update (base update)
    [ghstack-poisoned]

--- torchtitan/models/common/__init__.py
+++ torchtitan/models/common/__init__.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .activation import ActivationFn, SiTUGLU, SwiGLU
 from .attention import (
     BaseQKVLinear,
     create_attention_mask,
@@ -71,6 +72,9 @@ __all__ = [
     "ScaledBiasRowwiseLinear",
     "ScaledDotProductAttention",
     "SiLU",
+    "ActivationFn",
+    "SiTUGLU",
+    "SwiGLU",
     "TransformerBlock",
     "VarlenAttention",
     "VarlenMetadata",
