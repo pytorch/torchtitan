ebee695d4e29c4131f9a8a0a4e477b2456179a95
Sanket Jayant Purandare <sanketpurandare@meta.com>
2026-09-13 14:41:04 -0700

    [UPDATE] Update
    [ghstack-poisoned]

--- torchtitan/models/kimi_k3/sharding.py
+++ torchtitan/models/kimi_k3/sharding.py
@@ -92,9 +92,8 @@ def set_kimi_k3_sharding_config(
                 # without sequence parallel.
                 enable_sp=enable_sp,
                 expert_param_layout={
-                    "w1_EFD": spmd.S(1),
+                    "w13_E2FD": spmd.S(2),
                     "w2_EDF": spmd.S(2),
-                    "w3_EFD": spmd.S(1),
                 },
             )
 
