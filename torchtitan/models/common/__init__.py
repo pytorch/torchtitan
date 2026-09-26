c05477445eb6a3a0c67d02bb7ed8c9a6a3015b06
JessicaZhong <jessicazhong@meta.com>
2026-09-25 17:13:21 -0700

    [Perf] Compile gated RMSNorm by default

--- torchtitan/models/common/__init__.py
+++ torchtitan/models/common/__init__.py
@@ -7,6 +7,7 @@
 from .activation import (
     BinaryActivationFn,
     Sigmoid,
+    SiLU,
     SiTUGLU,
     Softmax,
     SqrtSoftplus,
@@ -42,16 +43,8 @@ from .linear import (
 )
 from .moe import MicrobatchWiseLoadBalanceLoss, MoE
 from .multimodal import MultimodalModel
-from .nn_modules import (
-    Conv1d,
-    Conv2d,
-    GELU,
-    GroupNorm,
-    Identity,
-    LayerNorm,
-    RMSNorm,
-    SiLU,
-)
+from .nn_modules import Conv1d, Conv2d, GELU, GroupNorm, Identity, LayerNorm, RMSNorm
+from .norm import GatedRMSNorm
 from .rope import ComplexRoPE, CosSinRoPE, RoPE
 
 __all__ = [
@@ -69,6 +62,7 @@ __all__ = [
     "FlexInnerAttention",
     "QKVLinear",
     "GELU",
+    "GatedRMSNorm",
     "get_causal_mask_mod",
     "get_document_mask_mod",
     "get_efficient_causal_mask_mod_for_packed_document",
